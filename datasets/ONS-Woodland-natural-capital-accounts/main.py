8cdef033e130fdd4e09c2089a863355131993dbd
chunpang <chun.pang@ons.gov.uk>
2021-05-19 14:39:09 +0100

    Added initial dataset settings for stage-1 transform task

--- datasets/ONS-Woodland-natural-capital-accounts/main.py
+++ datasets/ONS-Woodland-natural-capital-accounts/main.py
@@ -5,3 +5,26 @@
 import pandas as pd
 from gssutils import *
 import json
+
+info = json.load(open('info.json'))
+landingPage = info['landingPage']
+landingPage
+
+scraper = Scraper(seed='info.json')
+scraper
+
+trace = TransformTrace()
+cubes = Cubes('info.json')
+
+dist = scraper.distribution(latest=True, mediaType=Excel)
+datasetTitle = info['title']
+dist
+datasetTitle
+
+tabs_name = ['Physical flows', 'Annual value', 'Asset value']
+tabs = {tab: tab for tab in dist.as_databaker() if tab.name in tabs_name}
+if len(set(tabs_name) - {x.name for x in tabs}) != 0:
+    raise ValueError(f'Aborting. A tab named {set(tabs_name) - {x.name for x in tabs} } required but not found')
+
+for tab in tabs:
+    print(tab.name)
