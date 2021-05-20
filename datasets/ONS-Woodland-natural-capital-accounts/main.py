a3da2434c51ab957ee278242c5784bf1aea81b61
chunpang <chun.pang@ons.gov.uk>
2021-05-20 11:21:00 +0100

    Added initial transform trace functions

--- datasets/ONS-Woodland-natural-capital-accounts/main.py
+++ datasets/ONS-Woodland-natural-capital-accounts/main.py
@@ -26,5 +26,61 @@ tabs = {tab: tab for tab in dist.as_databaker() if tab.name in tabs_name}
 if len(set(tabs_name) - {x.name for x in tabs}) != 0:
     raise ValueError(f'Aborting. A tab named {set(tabs_name) - {x.name for x in tabs} } required but not found')
 
+
+def left(s, amount):
+    return s[:amount]
+
+
+def right(s, amount):
+    return s[-amount:]
+
+
+def mid(s, offset, amount):
+    return s[offset:offset+amount]
+
+
+def cellLoc(cell):
+    return right(str(cell), len(str(cell)) - 2).split(" ", 1)[0]
+
+
+def col2num(col):
+    num = 0
+    for c in col:
+        if c in string.ascii_letters:
+            num = num * 26 + (ord(c.upper()) - ord('A')) + 1
+    return num
+
+
+def colnum_string(n):
+    string = ""
+    while n > 0:
+        n, remainder = divmod(n - 1, 26)
+        string = chr(65 + remainder) + string
+    return string
+
+
+def excelRange(bag):
+    xvalues = []
+    yvalues = []
+    for cell in bag:
+        coordinate = cellLoc(cell)
+        xvalues.append(''.join([i for i in coordinate if not i.isdigit()]))
+        yvalues.append(int(''.join([i for i in coordinate if i.isdigit()])))
+    high = 0
+    low = 0
+    for i in xvalues:
+        if col2num(i) >= high:
+            high = col2num(i)
+        if low == 0:
+            low = col2num(i)
+        elif col2num(i) < low:
+            low = col2num(i)
+        highx = colnum_string(high)
+        lowx = colnum_string(low)
+    highy = str(max(yvalues))
+    lowy = str(min(yvalues))
+    return '{' + lowx + lowy + '-' + highx + highy + '}'
+
+
 for tab in tabs:
     print(tab.name)
