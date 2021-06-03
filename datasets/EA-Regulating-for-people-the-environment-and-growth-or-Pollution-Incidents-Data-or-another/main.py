223d9c0f259009e3a02a8315ab0380c26249c215
chunpang <chun.pang@ons.gov.uk>
2021-06-03 14:38:01 +0100

    Added initial dataset settings

--- datasets/EA-Regulating-for-people-the-environment-and-growth-or-Pollution-Incidents-Data-or-another/main.py
+++ datasets/EA-Regulating-for-people-the-environment-and-growth-or-Pollution-Incidents-Data-or-another/main.py
@@ -5,3 +5,18 @@
 import pandas as pd
 from gssutils import *
 import json
+
+info = json.load(open('info.json'))
+landingPage = info['landingPage']
+landingPage
+
+scraper = Scraper(seed='info.json')
+scraper
+
+trace = TransformTrace()
+cubes = Cubes('info.json')
+
+dist = scraper.distribution(mediaType=ODS, latest=True)
+datasetTitle = info['title']
+dist
+datasetTitle
