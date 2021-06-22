7500e320b9aa61eb4ff1113f6a647b8e1c4899bc
Osamede Asemota <osamede.asemota@gsscogs.uk>
2021-06-22 10:03:18 +0100

    Transform done in line with spec

---
+++ datasets/BEIS-2019-UK-greenhouse-gas-emissions-final-figures-dataset-of-emissions-by-end-user/main.py
@@ -0,0 +1,37 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.10.2
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+import json
+import pandas as pd
+from gssutils import *
+
+cubes = Cubes('info.json')
+
+info = json.load(open('info.json'))
+metadata = Scraper(seed='info.json')
+
+distribution = metadata.distributions[-1]
+
+title = distribution.title
+metadata.dataset.title = title
+
+df = distribution.as_pandas(encoding='ISO-8859-1')
+
+df.drop(df.columns[df.columns.str.contains('Unnamed',case = False)],axis = 1, inplace = True)
+df.rename(columns={'TerritoryName' : 'Geography Code', 'EmissionUnits' : 'Emission Units'}, inplace=True)
+
+df['Emission'] = df['Emission'].astype(float).round(5)
+
+cubes.add_cube(metadata, df, metadata.dataset.title)
+cubes.output_all()
