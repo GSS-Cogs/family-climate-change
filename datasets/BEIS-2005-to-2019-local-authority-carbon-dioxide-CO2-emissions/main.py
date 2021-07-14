923e85f3bc9fd37b890e8a5a27e314947a3cecd5
Osamede Asemota <osamede.asemota@gsscogs.uk>
2021-07-14 10:19:18 +0100

    BEIS-2005-to-2019-local-authority-carbon-dioxide-CO2-emissions (#25)
    * revising and including correct landing page
    
    * transforming data in line with spec

---
+++ datasets/BEIS-2005-to-2019-local-authority-carbon-dioxide-CO2-emissions/main.py
@@ -0,0 +1,52 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.10.2
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+# ## BEIS-2005-to-2019-local-authority-carbon-dioxide-CO2-emissions
+
+import json
+import pandas as pd
+from gssutils import *
+
+cubes = Cubes('info.json')
+
+info = json.load(open('info.json'))
+landingPage = info['landingPage']
+
+metadata = Scraper(seed='info.json')
+
+distribution = metadata.distribution(mediaType='text/csv')
+
+metadata.dataset.title = distribution.title
+
+df = distribution.as_pandas()
+
+df.drop(columns=df.columns.values.tolist()[0:6], axis=1, inplace=True)
+df.rename(columns={'Calendar Year': 'Year'}, inplace=True)
+
+for col in df.columns.values.tolist()[-4:]:
+    if col == 'Mid-year Population (thousands)':
+        df[col] = df[col].astype(str).astype(float).round(3)
+    else:
+        df[col] = df[col].astype(str).astype(float).round(2)
+
+for col in ['LA CO2 Sector', 'LA CO2 Sub-sector']:
+    try:
+        df[col] = df[col].apply(pathify)
+    except Exception as err:
+        raise Exception('Failed to pathify column "{}".'.format(col)) from err
+
+df = df.fillna(' ')
+
+cubes.add_cube(metadata, df, metadata.dataset.title)
+cubes.output_all()
