507e7a65c7f82588e3cf9b16222d28aeb548a281
Osamede Asemota <osamede.asemota@gsscogs.uk>
2021-07-27 16:57:24 +0100

    renaming columns and writing output files to out folder

--- datasets/Met-Office-Modelling-Rainfall/main.py
+++ datasets/Met-Office-Modelling-Rainfall/main.py
@@ -1,64 +1,40 @@
-#%%
-import re
+# +
+
 import pandas as pd 
 import json 
 from pathlib import Path
-from typing import Union, Sequence, Any, List
-from unidecode import unidecode
+# from typing import Union, Sequence, Any, List
+# from unidecode import unidecode
+from gssutils import *
+# -
 
-#%%
-#def pathify(label):
- #   """
-  #    Convert a label into something that can be used in a URI path segment.
-   # """
-    #return re.sub(r'-$', '',
-     #             re.sub(r'-+', '-',
-      #                   re.sub(r'[^\w/]', '-', unidecode(label).lower())))
+#
 
+df = pd.read_csv("raw.csv")
+df.drop(columns='daycount', axis=1, inplace=True)
 
+df = pd.melt(df, id_vars=['period-start'])
+df.rename(columns={'period-start': 'Year',
+						'variable': 'Geography',
+						'value': 'Rainfall'
+						}, inplace=True)
 
-#pd.melt(rainfall, id_vars=['period-start'])
+df['Year'] = pd.to_datetime(df['Year']).dt.strftime('%Y')
 
-
-
-
-#%%
-#rainfall["period_start"]= rainfall["period_start"].astype(str).apply(pathify) 
-#rainfall["east scotland"]= rainfall["east scotland"].astype(str).apply(pathify) 
-#rainfall["north scotland"]= rainfall["north scotland"].astype(str).apply(pathify) 
-#rainfall["west scotland"]= rainfall["west scotland"].astype(str).apply(pathify) 
-#rainfall["northern ireland"]= rainfall["northern ireland"].astype(str).apply(pathify) 
-#rainfall["east midlands"]= rainfall["east midlands"].astype(str).apply(pathify) 
-#rainfall["east of england"]= rainfall["east of england"].astype(str).apply(pathify) 
-#rainfall["london"]= rainfall["london"].astype(str).apply(pathify) 
-#rainfall["north east england"]= rainfall["north east england"].astype(str).apply(pathify) 
-#rainfall["north west england"]= rainfall["north west england"].astype(str).apply(pathify) 
-#rainfall["south east england"]= rainfall["south east england"].astype(str).apply(pathify) 
-#rainfall["south west england"]= rainfall["south west england"].astype(str).apply(pathify) 
-#rainfall["wales"]= rainfall["wales"].astype(str).apply(pathify) 
-#rainfall["west midlands"]= rainfall["west midlands"].astype(str).astype(str).apply(pathify) 
-#rainfall["daycount"]= rainfall["daycount"].astype(str).apply(pathify) 
-
-#%%
-rainfall = pd.read_csv("raw.csv")
+df['Rainfall'] = df['Rainfall'].astype(str).astype(float).round(2)
 
 
-rainfall = rainfall.drop(columns='daycount')
+out = Path('out')
+out.mkdir(exist_ok=True)
+df.to_csv(out/'monthly-rainfall.csv', index = False)
 
-rainfall = pd.melt(rainfall, id_vars=['period-start'])
-
-rainfall.to_csv("monthly-rainfall.csv",index = False) 
-
-
-
-# %%
 ##No scraper present so we have created this manually 
 from gssutils.csvw.mapping import CSVWMapping
 with open('info.json') as f:
     info_json = json.load(f)
 csvw_mapping = CSVWMapping()
 csvw_mapping.set_mapping(info_json)
-csvw_mapping.set_csv(Path("monthly-rainfall.csv"))
+csvw_mapping.set_csv(out/"monthly-rainfall.csv")
 csvw_mapping.set_dataset_uri(f"http://gss-data.org.uk/data/gss_data/climate-change/{info_json['id']}")
-csvw_mapping.write(Path("rainfall.csv-metadata.json"))
-# %%
+csvw_mapping.write(out/'rainfall.csv-metadata.json')
+
