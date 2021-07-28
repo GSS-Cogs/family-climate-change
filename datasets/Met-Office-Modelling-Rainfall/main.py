194d24b3f6f8beeb580869b7b2526dbce7b10c16
Osamede Asemota <osamede.asemota@gsscogs.uk>
2021-07-28 12:09:03 +0100

    removing empty cells

--- datasets/Met-Office-Modelling-Rainfall/main.py
+++ datasets/Met-Office-Modelling-Rainfall/main.py
@@ -1,35 +1,28 @@
-# +
-
-import pandas as pd 
 import json 
-from pathlib import Path
-# from typing import Union, Sequence, Any, List
-# from unidecode import unidecode
+import pandas as pd 
 from gssutils import *
-# -
-
-#
+from pathlib import Path
+from gssutils.csvw.mapping import CSVWMapping
 
 df = pd.read_csv("raw.csv")
 df.drop(columns='daycount', axis=1, inplace=True)
 
 df = pd.melt(df, id_vars=['period-start'])
 df.rename(columns={'period-start': 'Year',
-						'variable': 'Geography',
-						'value': 'Rainfall'
-						}, inplace=True)
+'variable': 'Geography',
+'value': 'Rainfall'
+}, inplace=True)
 
 df['Year'] = pd.to_datetime(df['Year']).dt.strftime('%Y')
 
 df['Rainfall'] = df['Rainfall'].astype(str).astype(float).round(2)
 
-
 out = Path('out')
 out.mkdir(exist_ok=True)
 df.to_csv(out/'monthly-rainfall.csv', index = False)
 
-##No scraper present so we have created this manually 
-from gssutils.csvw.mapping import CSVWMapping
+# ## No scraper present so we have created this manually
+
 with open('info.json') as f:
     info_json = json.load(f)
 csvw_mapping = CSVWMapping()
@@ -37,4 +30,3 @@ csvw_mapping.set_mapping(info_json)
 csvw_mapping.set_csv(out/"monthly-rainfall.csv")
 csvw_mapping.set_dataset_uri(f"http://gss-data.org.uk/data/gss_data/climate-change/{info_json['id']}")
 csvw_mapping.write(out/'rainfall.csv-metadata.json')
-
