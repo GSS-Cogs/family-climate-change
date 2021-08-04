751a0f295389a71332008a0652e10bda3d8b8851
LPerryman <45283878+LPerryman@users.noreply.github.com>
2021-08-04 14:38:20 +0100

    Update main.py

--- datasets/Met-Office-Modelling-Rainfall/main.py
+++ datasets/Met-Office-Modelling-Rainfall/main.py
@@ -11,7 +11,7 @@ df.drop(columns='daycount', axis=1, inplace=True)
 df = pd.melt(df, id_vars=['period-start'])
 df.rename(columns={'period-start': 'Month',
                 'variable': 'Geography',
-                'value','Value'
+                'value': 'Value'
                 }, inplace=True)
 
 df['Month'] = pd.to_datetime(df['Month'], dayfirst=True).dt.strftime('%Y-%m')
