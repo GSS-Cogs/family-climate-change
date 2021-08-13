3f1680f3db21dd9bcb8c9e3396798b91efeb1cec
AndrewtGSScogs <75667265+AndrewtGSScogs@users.noreply.github.com>
2021-08-13 10:36:59 +0000

    reverting changes

--- datasets/BEIS-2005-to-2019-local-authority-carbon-dioxide-CO2-emissions/main.py
+++ datasets/BEIS-2005-to-2019-local-authority-carbon-dioxide-CO2-emissions/main.py
@@ -33,13 +33,12 @@ df = distribution.as_pandas()
 df.drop(columns=df.columns.values.tolist()[0:6], axis=1, inplace=True)
 df.drop(columns=['Mid-year Population (thousands)', 'Area (km2)'], axis=1, inplace=True)
 df.rename(columns={'Calendar Year': 'Year',
-					'Territorial emissions (kt CO2)':'Territorial Emissions',
-					'Emissions within the scope of influence of LAs (kt CO2)': 'Emissions within the scope of influence of LAs'
-                  
-                    
+					'Territorial emissions (kt CO2)':'Territorial emissions',
+					'Emissions within the scope of influence of LAs (kt CO2)': 'Emissions within the scope of influence of LAs',
+                    'value': 'Value'
 			}, inplace=True)
 
-val_vars = ['Territorial Emissions', 'Emissions within the scope of influence of LAs']
+val_vars = ['Territorial emissions', 'Emissions within the scope of influence of LAs']
 other_vars = df.columns.difference(val_vars)
 df = pd.melt(df, id_vars= other_vars, value_vars= val_vars, var_name= 'Measure Type')
  
@@ -54,9 +53,7 @@ for col in ['LA CO2 Sector', 'LA CO2 Sub-sector', 'Measure Type']:
 
 df = df.fillna('unallocated consumption')
 
-df.rename(columns={'value': 'Value',
-					}, inplace=True)
-
 df['Units'] = 'kt-co2'
+
 cubes.add_cube(metadata, df, metadata.dataset.title)
 cubes.output_all()
