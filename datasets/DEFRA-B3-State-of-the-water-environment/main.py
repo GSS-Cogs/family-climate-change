99258aeee5099edc57311b75afd6298fa3661c57
Osamede Asemota <osamede.asemota@gsscogs.uk>
2021-09-09 17:39:32 +0100

    transform completed according to spec

--- datasets/DEFRA-B3-State-of-the-water-environment/main.py
+++ datasets/DEFRA-B3-State-of-the-water-environment/main.py
@@ -5,9 +5,10 @@
 #       extension: .py
 #       format_name: light
 #       format_version: '1.5'
-#       jupytext_version: 1.11.4
+#       jupytext_version: 1.11.5
 #   kernelspec:
-#     display_name: Python 3.8.8 64-bit
+#     display_name: Python 3 (ipykernel)
+#     language: python
 #     name: python3
 # ---
 
@@ -23,9 +24,27 @@ landingPage = info['landingPage']
 landingPage
 
 metadata = Scraper(seed="info.json")
-metadata
+metadata.select_dataset(title = lambda x: "B3" in x) 
 
 distribution = metadata.distribution(mediaType="text/csv")
-distribution
+metadata.dataset.title = distribution.title
+metadata.dataset.family = 'DEFRA'
 
-df = distribution.as_pandas()
+df = distribution.as_pandas().fillna('')
+
+df['Label'] = df['Year'].str.extract(r'([^(\d+)]+)')
+df['Year'] = df['Year'].str.extract(r'(\d+)')
+
+# + tags=[]
+df = df[['Year', 'Label', 'Series', 'Component', 'Status category', 'Value']]
+# -
+
+for col in df.columns.to_list()[1:-1]:
+    try:
+        df[col] = df[col].apply(pathify)
+    except Exception as err:
+        raise Exception('Failed to pathify column "{}".'.format(col)) from err
+
+# + tags=[]
+cubes.add_cube(metadata, df, metadata.dataset.title)
+cubes.output_all()
