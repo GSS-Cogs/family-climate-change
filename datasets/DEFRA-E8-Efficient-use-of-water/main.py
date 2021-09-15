f301e7482bb9dc92060a79c3d77536d0fa2b3b46
RedWalters <jonathan.j.walters@Gmail.com>
2021-09-15 12:18:48 +0100

    str(x['Value'])

--- datasets/DEFRA-E8-Efficient-use-of-water/main.py
+++ datasets/DEFRA-E8-Efficient-use-of-water/main.py
@@ -35,11 +35,11 @@ df['Year'] = df.apply(lambda x: 'government-year/' + x['Year'], axis = 1)
 
 df = df.rename(columns={'Year' : 'Period'})
 
-df['Unit'] = df.apply(lambda x: 'megalitres-per-day' if 'E8A' in x['Series'] else 'litres-per-person-per-day', axis = 1)
 df['Measure Type'] = df.apply(lambda x: 'water-leakage' if 'E8A' in x['Series'] else 'water-consumption', axis = 1)
+df['Unit'] = df.apply(lambda x: 'megalitres-per-day' if 'E8A' in x['Series'] else 'litres-per-person-per-day', axis = 1)
 
-df['Marker'] = df.apply (lambda x: 'not-available' if 'not-available' in x['Value'] else '', axis = 1)
-df['Value'] = df.apply (lambda x: '' if 'not-available' in x['Value'] else x['Value'], axis = 1)
+df['Marker'] = df.apply (lambda x: 'not-available' if 'not-available' in str(x['Value']) else '', axis = 1)
+df['Value'] = df.apply (lambda x: '' if 'not-available' in str(x['Value']) else x['Value'], axis = 1)
 
 df = df.drop(['Series'], axis = 1)
 
