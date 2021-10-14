80a21009eed657d3c2c668aea673861bf3ffcafe
RedWalters <jonathan.j.walters@Gmail.com>
2021-10-14 14:08:24 +0100

    stage 2 mapping and codelists

--- datasets/DEFRA-Carbon-Footprint-Summary-Source-Region-90-18/main.py
+++ datasets/DEFRA-Carbon-Footprint-Summary-Source-Region-90-18/main.py
@@ -1,21 +1,13 @@
-# ---
-# jupyter:
-#   jupytext:
-#     text_representation:
-#       extension: .py
-#       format_name: light
-#       format_version: '1.5'
-#       jupytext_version: 1.11.5
-#   kernelspec:
-#     display_name: Python 3 (ipykernel)
-#     language: python
-#     name: python3
-# ---
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[15]:
+
 
 # ## DEFRA-Carbon-Footprint-Summary-Source-Region-90-18
 
-import json 
-import pandas as pd 
+import json
+import pandas as pd
 from gssutils import *
 from pathlib import Path
 import shutil
@@ -26,30 +18,41 @@ df = pd.read_csv('raw.csv', skiprows=2)
 df = df.drop(columns='Unnamed: 0')
 df.rename(columns={'Unnamed: 1': 'Period'}, inplace=True)
 
-# +
+
+# In[16]:
+
+
 df = pd.melt(df, id_vars=['Period'], var_name='Country Code', value_name='Value')
 df = df.replace({'Total' : 'All'})
 df['Period'] = df['Period'].map(lambda x: 'year/' + str(x))
 df['Value'] = df['Value'].astype(str).astype(float).round(3)
-df['Unit'] = 'Ktonnes CO2e'
+df['Unit'] = 'kt-co2e'
 
-df['Country Code'] = df['Country Code'].apply(pathify)
 df = df[['Period', 'Country Code', 'Value', 'Unit']]
-# -
+
+
+# In[17]:
+
+
 out = Path('out')
 out.mkdir(exist_ok=True)
-df.to_csv(out/'summary-source-region.csv', index = False)
+df.to_csv(out/'observations.csv', index = False)
 
-# Metadata.json file is created manually as dataset was received as as raw csv file 
+# Metadata.json file is created manually as dataset was received as as raw csv file
 with open('info.json') as f:
     info_json = json.load(f)
 csvw_mapping = CSVWMapping()
 csvw_mapping.set_mapping(info_json)
-csvw_mapping.set_csv(out/'summary-source-region.csv')
+csvw_mapping.set_csv(out/'observations.csv')
 csvw_mapping.set_dataset_uri(f"http://gss-data.org.uk/data/gss_data/climate-change/{info_json['id']}")
-csvw_mapping.write(out/'summary-source-region.csv-metadata.json')
+csvw_mapping.write(out/'observations.csv-metadata.json')
 
 # metadata.trig file is manually created as dataset was received as as raw csv file
-shutil.copy("summary-source-region.csv-metadata.trig", out/"summary-source-region.csv-metadata.trig")
+shutil.copy("observations.csv-metadata.trig", out/"observations.csv-metadata.trig")
+
+
+# In[17]:
+
+
 
 
