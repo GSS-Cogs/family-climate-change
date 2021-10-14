1f980cd84e8fbdd8e55299144248472414a57041
Shannon Black <shannon1995black@gmail.com>
2021-10-14 12:39:02 +0100

    mapping and codelist creation for #76

--- datasets/ONS-Energy-use-total/main.py
+++ datasets/ONS-Energy-use-total/main.py
@@ -35,42 +35,30 @@ for tab in tabs:
     observations = year.fill(DOWN).is_not_blank()
     source = tab.excel_ref('A4').expand(DOWN).is_bold()
     industry_section = tab.excel_ref('A4').expand(DOWN).is_not_blank()
-    if "(Mtoe)" in tab.name: 
-        unit = 'Millions of tonnes of oil equivalent (Mtoe)'
-    if "(PJ)" in tab.name: 
-        unit = 'Energy Consumption in Petajoules (PJ)'
+    unit = tab.excel_ref('AD3')
     
     dimensions = [
         HDim(year, 'Year', DIRECTLY, ABOVE),
-        HDim(source, "Source", CLOSEST, ABOVE),
+        HDim(source, "Energy Consumption Source", CLOSEST, ABOVE),
         HDim(industry_section, "Industry Section", DIRECTLY, LEFT),
-        HDimConst('Unit', unit),
+        HDim(unit, 'Unit', CLOSEST, ABOVE),
     ]
     tidy_sheet = ConversionSegment(tab, dimensions, observations)
     #savepreviewhtml(tidy_sheet, fname = tab.name+ "Preview.html")
     df = tidy_sheet.topandas()
     tidied_sheets.append(df)
-    
+
 
 df = pd.concat(tidied_sheets, sort=True)
 df.rename(columns={'OBS' : 'Value'}, inplace=True)
 df['Year'] = df['Year'].astype(str).replace('\.0', '', regex=True)
 
-# Notes 
-# - Source values have been taken from the bold values held within cells a4 down: 
-#        'Total energy consumption of primary fuels and equivalents',
-#        'Direct1 use of energy from fossil fuels',
-#        'Energy from other sources',
-#        'Direct use of energy including other sources',
-#        'Reallocated1 use of energy'
-#        
-# - Industry section values have been taken from all cells from a4 down including those used inside the Source diemension aswell. I am not sure if this is ok / they need to be reworded as a 'total' situation. 
-#
-# - each tab has a different unit which I have populated the unit dimension with. 
-
 df['Industry Section'] = df['Industry Section'].apply(pathify)
-df['Source'] = df['Source'].apply(pathify)
-df = df[['Year', 'Source', 'Industry Section', 'Value', 'Unit']]
+df['Energy Consumption Source'] = df['Energy Consumption Source'].apply(pathify)
+df['Measure Type'] = 'gross-caloric-values'
+df["Unit"]= df['Unit'].str.extract('.*\((.*)\).*')
+df['Unit'] = df['Unit'].apply(pathify)
+df = df[['Year', 'Energy Consumption Source', 'Industry Section', 'Value', 'Measure Type', 'Unit']]
 cubes.add_cube(metadata, df.drop_duplicates(), title)
 cubes.output_all()
 df
