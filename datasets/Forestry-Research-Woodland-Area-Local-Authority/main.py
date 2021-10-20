5b6c1ae41f73f70a5f34882ba2b1c2c83f96565f
Shannon Black <shannon1995black@gmail.com>
2021-10-20 16:59:15 +0100

    Transformation, mapping, codelist creation #137

--- datasets/Forestry-Research-Woodland-Area-Local-Authority/main.py
+++ datasets/Forestry-Research-Woodland-Area-Local-Authority/main.py
@@ -7,7 +7,8 @@
 #       format_version: '1.5'
 #       jupytext_version: 1.11.5
 #   kernelspec:
-#     display_name: Python 3.8.8 64-bit
+#     display_name: Python 3 (ipykernel)
+#     language: python
 #     name: python3
 # ---
 
@@ -20,28 +21,53 @@ from pathlib import Path
 import shutil
 from gssutils.csvw.mapping import CSVWMapping
 
+# +
 df = pd.read_csv('raw.csv', skiprows=2, encoding='ISO-8859-1')
+# 17 blank rows at bottom need to be dropped. 
+n = 17
+df.drop(df.tail(n).index, inplace = True)
 
-columns = df.columns.tolist()
-df.dropna(subset=columns, inplace=True)
-df.rename(columns={'Local_authority_Area' : 'Local Authority Area',
-					'Standard_area_measurement _hectares' : 'Standard Area Measurement (hectares)',
-					'Woodland_hectares' : 'Woodland (hectares)',
-					'Woodland_%': 'Woodland %'
-				}, inplace=True)
+#Measure types / units to be extracted 
+val_vars =[ "Standard_area_measurement _hectares",
+"Woodland_hectares", 
+"Woodland_%"]
+other_vars = df.columns.difference(val_vars)
+df = pd.melt(
+    df, 
+    id_vars=other_vars, 
+    value_vars=val_vars, 
+    var_name='Measure Type',
+    value_name='Value'
+)
+df.rename(columns={'Local_authority_Area': 'Local Authority Area'}, inplace=True)
+#add unit dimension based of Measure Type 
+df['Unit'] = df['Measure Type'].apply(lambda x: 'hectares' if 'hectares' in x else 'percentage')
+df = df.replace({'Measure Type' : {'Standard_area_measurement _hectares' : "standard-area-measurement",
+                                  'Woodland_hectares' : 'woodland',
+                                  'Woodland_%' : 'woodland'}})
+df['Year'] = '2019'
+df = df[['Year', 'Code', 'Value', 'Measure Type', 'Unit']]
+df.rename(columns={'Code' : 'Local Authority Area'}, inplace=True)
+# -
 
 out = Path('out')
 out.mkdir(exist_ok=True)
-df.to_csv(out/'observations.csv', index = False)
+df.to_csv(out/'forestry-research-woodland-area-local-authority.csv', index = False)
 
-# Metadata.json file is created manually as dataset was received as as raw csv file
+# ## No scraper present so we have created this manually
+
+# +
 with open('info.json') as f:
     info_json = json.load(f)
 csvw_mapping = CSVWMapping()
 csvw_mapping.set_mapping(info_json)
-csvw_mapping.set_csv(out/'observations.csv')
+csvw_mapping.set_csv(out/"forestry-research-woodland-area-local-authority.csv")
 csvw_mapping.set_dataset_uri(f"http://gss-data.org.uk/data/gss_data/climate-change/{info_json['id']}")
-csvw_mapping.write(out/'observations.csv-metadata.json')
+csvw_mapping.write(out/'forestry-research-woodland-area-local-authority.csv-metadata.json')
 
-# metadata.trig file is created manually as dataset was received as as raw csv file
-shutil.copy("observations.csv-metadata.trig", out/"observations.csv-metadata.trig")
+shutil.copy("forestry-research-woodland-area-local-authority.trig", out/"forestry-research-woodland-area-local-authority.trig")
+# -
+
+
+
+
