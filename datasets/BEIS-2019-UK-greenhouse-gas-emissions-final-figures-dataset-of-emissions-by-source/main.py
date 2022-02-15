2f46741427ace07a3a834683169c77fedf111f44
“osamede20” <osamede.asemota@gsscogs.uk>
2022-02-15 12:19:21 +0000

    update with csvcubed tool

--- datasets/BEIS-2019-UK-greenhouse-gas-emissions-final-figures-dataset-of-emissions-by-source/main.py
+++ datasets/BEIS-2019-UK-greenhouse-gas-emissions-final-figures-dataset-of-emissions-by-source/main.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 # ---
 # jupyter:
 #   jupytext:
@@ -5,7 +6,7 @@
 #       extension: .py
 #       format_name: light
 #       format_version: '1.5'
-#       jupytext_version: 1.10.2
+#       jupytext_version: 1.13.1
 #   kernelspec:
 #     display_name: Python 3
 #     language: python
@@ -14,46 +15,32 @@
 
 # ## BEIS-2019-UK-greenhouse-gas-emissions-final-figures-dataset-of-emissions-by-source
 
-import json
 import pandas as pd
 from gssutils import *
 
-cubes = Cubes('info.json')
+metadata = Scraper(seed='ghg_emissions_by_source-info.json')
 
-info = json.load(open('info.json'))
-landingPage = info['landingPage']
-
-metadata = Scraper(seed='info.json')
-
-distribution = metadata.distributions[-2]
-
-title = distribution.title
-metadata.dataset.title = title
+distribution = metadata.distribution(mediaType='text/csv', latest=True, title = lambda x: 'UK greenhouse gas emissions: final figures – dataset of emissions by source' in x)
 
 df = distribution.as_pandas(encoding='ISO-8859-1').fillna(' ')
-
-df.loc[(df['National Communication Sub-sector'] == ' '), 'National Communication Sub-sector'] = 'Not Applicable'
-
-df.drop(columns=['TerritoryName', 'EmissionUnits'], axis=1, inplace=True)
+df.loc[(df['National Communication Sub-sector'] == '(blank)'), 'National Communication Sub-sector'] = 'Not Applicable'
+df.drop(columns='TerritoryName', axis=1, inplace=True)
+df.rename(columns={'ActivityName' : 'Activity Name'}, inplace=True)
 df.drop(df.columns[df.columns.str.contains('Unnamed',case = False)],axis = 1, inplace = True)
 
-df.rename(columns={'ActivityName' : 'Activity Name',
-                  'Emission' : 'Value'}, 
-            inplace=True)
 
-df['Value'] = df['Value'].astype(float).round(5)
-
-for col in df.columns.values.tolist():
-    if col in ['GHG', 'GHG Grouped', 'IPCC Code', 'Year', 'Value']: 
-        continue
-    try:
-        df[col] = df[col].apply(pathify)
-    except Exception as err:
-        raise Exception('Failed to pathify column "{}".'.format(col)) from err
-
-# Fix BEIS' use of slashes in some columns:
+# Fixing BEIS' use of slashes in some columns:
 df['National Communication Category'] = df['National Communication Category'].str.replace('/', '-')
 df['Source'] = df['Source'].str.replace('/', '-').str.replace('-+', '-', regex = True)
 
-cubes.add_cube(metadata, df, metadata.dataset.title)
-cubes.output_all()
+df = pd.melt(df, id_vars=['GHG', 'GHG Grouped', 'IPCC Code', 'Year', 'National Communication Sector','National Communication Sub-sector', 'National Communication Category', 'Source', 'National Communication Fuel Group','National Communication Fuel', 'Activity Name'], value_vars=['Emissions (MtCO2e, AR4 GWPs)', 'Emissions (MtCO2e, AR5 GWPs)'], var_name='Measure', value_name='Value')
+df = df.sort_values(['GHG', 'GHG Grouped', 'IPCC Code', 'Year', 'National Communication Sector',
+			'National Communication Sub-sector', 'National Communication Category', 'Source', 'National Communication Fuel Group',
+			'National Communication Fuel', 'Activity Name'])
+df['Value'] = df['Value'].astype(float).round(5)	
+
+df['Measure'] = df['Measure'].str.replace('MtCO2e, ', '')
+
+df.to_csv('ghg_emissions_by_source-observations.csv')
+catalog_metadata = metadata.as_csvqb_catalog_metadata()
+catalog_metadata.to_json_file('ghg_emissions_by_source-catalog-metadata.json')
