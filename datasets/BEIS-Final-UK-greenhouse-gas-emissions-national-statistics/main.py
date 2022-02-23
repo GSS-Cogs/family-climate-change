8aae368cc5025a45dcf0c3d92f92fa6c0a4d657d
“osamede20” <osamede.asemota@gsscogs.uk>
2022-02-23 12:53:29 +0000

    revised in line with csvcubed tooling

--- datasets/BEIS-Final-UK-greenhouse-gas-emissions-national-statistics/main.py
+++ datasets/BEIS-Final-UK-greenhouse-gas-emissions-national-statistics/main.py
@@ -1,48 +1,26 @@
 #!/usr/bin/env python
 # coding: utf-8
+# ## BEIS-Final-UK-greenhouse-gas-emissions-national-statistics
 
-# In[205]:
-
-
+import json
+import pandas as pd
 from gssutils import *
 
-cubes = Cubes("info.json")
-
-scraper = Scraper(seed = "info.json")
-scraper
-
-
-# In[206]:
-
-
-distribution  = scraper.distribution(latest=True, title = lambda x:"2020 UK greenhouse gas emissions: provisional figures - data tables" in x)
-distribution
-
-
-# In[207]:
-
+metadata = Scraper(seed="info.json")
+distribution = metadata.distribution(
+    latest=True, mediaType="application/vnd.oasis.opendocument.spreadsheet", title=lambda x: "UK greenhouse gas emissions: provisional figures - data tables" in x)
 
 tabs = distribution.as_databaker()
-
 for tab in tabs:
     print(tab.name)
 
-
-# In[208]:
-
-
 sheets = []
-
 tabs = [tab for tab in tabs if 'Contents' not in tab.name]
-
 for tab in tabs:
-
-    if tab.name not in ['Table 3', 'Table 4']: #tables 3 and 4 are moving averages
-
-        remove = tab.filter(contains_string("2020 estimates")).expand(RIGHT).expand(DOWN)
-
+    if tab.name not in ['Table 3', 'Table 4']:  # tables 3 and 4 are moving averages
+        remove = tab.filter(contains_string(
+            "2020 estimates")).expand(RIGHT).expand(DOWN)
         cell = tab.excel_ref("A2")
-
         if tab.name not in ['Table 1', 'Table 2']:
             period = cell.fill(RIGHT).is_not_blank().is_not_whitespace()
             quarter = period.shift(DOWN).expand(RIGHT)
@@ -53,7 +31,8 @@ for tab in tabs:
         area = 'K02000001'
 
         if tab.name not in ['Table 2']:
-            ncSector = cell.fill(DOWN).is_not_blank().is_not_whitespace() - remove
+            ncSector = cell.fill(DOWN).is_not_blank(
+            ).is_not_whitespace() - remove
             fuel = 'all'
         else:
             ncSector = 'all'
@@ -63,13 +42,14 @@ for tab in tabs:
             measureType = 'greenhouse-gas-emissions'
             unit = 'millions-of-tonnes-of-co2-equivalent'
         elif tab.name in ['Table 2']:
-            measureType  = 'carbon-dioxide-emissions'
+            measureType = 'carbon-dioxide-emissions'
             unit = 'millions-of-tonnes-of-co2'
         elif tab.name in ['Table 6']:
-            measureType  = 'temperature-adjusted-greenhouse-gas-emissions'
+            measureType = 'temperature-adjusted-greenhouse-gas-emissions'
             unit = 'millions-of-tonnes-of-co2-equivalent'
 
-        observations = quarter.fill(DOWN).is_not_blank().is_not_whitespace() - remove
+        observations = quarter.fill(
+            DOWN).is_not_blank().is_not_whitespace() - remove
 
         if tab.name not in ['Table 2']:
             dimensions = [
@@ -78,7 +58,7 @@ for tab in tabs:
                 HDim(quarter, "Quarter", DIRECTLY, ABOVE),
                 HDim(ncSector, "National Communication Sector", DIRECTLY, LEFT),
                 HDimConst('Fuel', fuel),
-                HDimConst('Measure Type', measureType),
+                HDimConst('Measure', measureType),
                 HDimConst('Unit', unit)
             ]
         else:
@@ -88,56 +68,39 @@ for tab in tabs:
                 HDim(quarter, "Quarter", DIRECTLY, ABOVE),
                 HDimConst("National Communication Sector", ncSector),
                 HDim(fuel, 'Fuel', DIRECTLY, LEFT),
-                HDimConst('Measure Type', measureType),
+                HDimConst('Measure', measureType),
                 HDimConst('Unit', unit)
             ]
-
         tidy_sheet = ConversionSegment(tab, dimensions, observations)
         df = tidy_sheet.topandas()
-        savepreviewhtml(tidy_sheet,fname=tab.name + "Preview.html")
-
         sheets.append(df)
-
-
-# In[209]:
-
 
 df = pd.concat(sheets)
 
-df['Marker'] = df.apply(lambda x: 'provisional' if '(p)' in x['Period'] else '', axis = 1)
-df['Period'] = df.apply(lambda x: 'year/' + str(x['Period'])[:4] if 'Q' not in x['Quarter']                                                                 else 'quarter/' + str(x['Period'])[:4] + '-' + str(x['Quarter']), axis = 1)
-df = df.drop(columns = ['Quarter'])
+# +
+df['Marker'] = df.apply(
+    lambda x: 'provisional' if '(p)' in x['Period'] else '', axis=1)
+df['Period'] = df.apply(lambda x: 'year/' + str(x['Period'])[:4] if 'Q' not in x['Quarter']
+                        else 'quarter/' + str(x['Period'])[:4] + '-' + str(x['Quarter']), axis=1)
+df = df.drop(columns=['Quarter'])
 
-df = df.replace({'National Communication Sector' : {'     from power stations' : 'Power Stations',
-                                 '     other Energy supply' : 'Other Energy Supply',
-                                                    'LULUCF' : 'land-use-land-use-change-and-forestry'}})
+df = df.replace({'National Communication Sector': {'     from power stations': 'Power Stations',
+                                                   '     other Energy supply': 'Other Energy Supply',
+                                                   'LULUCF': 'land-use-land-use-change-and-forestry'}})
 
-indexNames = df[ df['Fuel'] == 'Total' ].index
-df.drop(indexNames, inplace = True)
+indexNames = df[df['Fuel'] == 'Total'].index
+df.drop(indexNames, inplace=True)
 
-df['National Communication Sector'] = df['National Communication Sector'].map(lambda x: pathify(x))
+df['National Communication Sector'] = df['National Communication Sector'].map(
+    lambda x: pathify(x))
 df['Fuel'] = df['Fuel'].map(lambda x: pathify(x))
 
-df = df.rename(columns = {'OBS' : 'Value'})
-
+df = df.rename(columns={'OBS': 'Value'})
 df['Value'] = df['Value'].map(lambda x: round(x, 1))
+df = df[['Period', 'Area', 'National Communication Sector',
+         'Fuel', 'Value', 'Marker', 'Measure', 'Unit']]
+# -
 
-df = df[['Period', 'Area', 'National Communication Sector', 'Fuel', 'Value', 'Marker', 'Measure Type', 'Unit']]
-
-df
-
-
-# In[210]:
-
-
-scraper.dataset.comment = """Final estimates of UK territorial greenhouse gas emissions, including provisional data for 2020"""
-
-cubes.add_cube(scraper, df.drop_duplicates(), scraper.dataset.title)
-cubes.output_all()
-
-
-# In[210]:
-
-
-
-
+df.to_csv("observations.csv", index=False)
+catalog_metadata = metadata.as_csvqb_catalog_metadata()
+catalog_metadata.to_json_file("catalog-metadata.json")
