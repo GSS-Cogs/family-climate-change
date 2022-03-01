f991586ca7a62d894e6a7ade8eee0e5a39059ba4
bailed22 <david.bailey@gsscogs.uk>
2022-03-01 17:09:53 +0000

    change to csvcubed pipeline

--- datasets/BEIS-Energy-Intensity-Extract/main.py
+++ datasets/BEIS-Energy-Intensity-Extract/main.py
@@ -5,25 +5,23 @@
 #       extension: .py
 #       format_name: light
 #       format_version: '1.5'
-#       jupytext_version: 1.11.5
+#       jupytext_version: 1.13.5
 #   kernelspec:
 #     display_name: Python 3 (ipykernel)
 #     language: python
 #     name: python3
 # ---
 
-import json 
 import pandas as pd 
 from gssutils import *
-from pathlib import Path
-import shutil
-from gssutils.csvw.mapping import CSVWMapping
 import numpy as np
+from csvcubed.models.cube.qb.catalog import CatalogMetadata
+from datetime import datetime
 
-# +
 df = pd.read_csv('raw.csv', encoding='ISO-8859-1')
+
 df = df.drop(columns=df.columns[13:17])
-    
+
 #Measure types / units to be extracted 
 val_vars =[ "Road Passenger Consumption (ktoe)", #(ROAD)
 "Output (billion passenger kilometres)", #(ROAD)
@@ -38,14 +36,13 @@ val_vars =[ "Road Passenger Consumption (ktoe)", #(ROAD)
 "Services Output", #(SERVICES)
 "Energy consumption per unit of output (ktoe).1", #(SERVICES)
           ]
-other_vars = df.columns.difference(val_vars)
-df = pd.melt(
-    df, 
-    id_vars=other_vars, 
-    value_vars=val_vars, 
-    var_name='Measure Type',
-    value_name='Value'
-)
+other_vars = df.columns.difference(val_vars)          
+
+# +
+
+df = pd.melt(df, id_vars=other_vars, value_vars=val_vars, var_name='Measure Type',value_name='Value')
+# -
+
 df['Sector'] =  df['Measure Type']
 sector_values = {
     "Road Passenger Consumption (ktoe)" : "road", 
@@ -61,9 +58,8 @@ sector_values = {
     "Services Output": "services",
     "Energy consumption per unit of output (ktoe).1": "services"
 }
-df['Sector'] = df['Sector'].replace(sector_values)
-# -
 
+df['Sector'] = df['Sector'].replace(sector_values)
 df["Unit"]= df['Measure Type'].str.extract('.*\((.*)\).*')
 df['Measure Type'] = df['Measure Type'].str.strip()
 df['Measure Type'] = df['Measure Type'].str.replace(r"\(.*\)","").str.strip()
@@ -74,21 +70,15 @@ df["Unit"].fillna("UNKNOWN", inplace = True)
 df['Unit'] = df['Unit'].apply(pathify)
 df = df.replace(r'^\s*$', np.nan, regex=True)
 df = df.dropna(subset=['Value'])
-df
 
-out = Path('out')
-out.mkdir(exist_ok=True)
-df.to_csv(out/'energy.csv', index = False)
+df['Value'] = pd.to_numeric(df['Value'], errors="raise", downcast="float")
+df["Value"] = df["Value"].astype(float).round(3)
 
-# ## No scraper present so we have created this manually
+df = df[['Year', 'Measure Type', 'Sector', 'Unit', 'Value']]
 
-# +
-with open('info.json') as f:
-    info_json = json.load(f)
-csvw_mapping = CSVWMapping()
-csvw_mapping.set_mapping(info_json)
-csvw_mapping.set_csv(out/"energy.csv")
-csvw_mapping.set_dataset_uri(f"http://gss-data.org.uk/data/gss_data/climate-change/{info_json['id']}")
-csvw_mapping.write(out/'energy.csv-metadata.json')
-
-shutil.copy("energy.csv-metadata.trig", out/"energy.csv-metadata.trig")
+df.to_csv('observations.csv', index=False)
+catalog_metadata = CatalogMetadata(
+    title = "Energy Intensity Extract",
+    description = "The publication provides the final extract of BEIS Energy Intensity for 2020"
+)
+catalog_metadata.to_json_file('catalog-metadata.json')
