3519679b92836c8fe6ca50a3f87c6724a1f5acce
bailed22 <david.bailey@gsscogs.uk>
2022-03-15 11:55:14 +0000

    change legacy pipeline to csvcubed pipeline

--- datasets/Met-Office-Annual-mean-rainfall-with-trends-actual/main.py
+++ datasets/Met-Office-Annual-mean-rainfall-with-trends-actual/main.py
@@ -1,40 +1,17 @@
-import json 
 import pandas as pd 
 from gssutils import *
-from pathlib import Path
-import shutil
-from gssutils.csvw.mapping import CSVWMapping
+from csvcubed.models.cube.qb.catalog import CatalogMetadata
 
 df = pd.read_csv("raw.csv")
-#df.drop(columns='daycount', axis=1, inplace=True)
 
 df = pd.melt(df, id_vars=['Year'])
-df.rename(columns={
-                'variable': 'Geography',
-                'value': 'Value'
-                }, inplace=True)
-
-#df['Month'] = pd.to_datetime(df['Month'], dayfirst=True).dt.strftime('%Y-%m')
-
-#df['Geography'].replace({'ondon': 'london'}, inplace=True)
+df.rename(columns={ 'variable': 'Geography','value': 'Value'}, inplace=True)
 df['Geography'] = df['Geography'].apply(pathify)
-#df['Rainfall'] = df['Rainfall'].astype(str).astype(float).round(2)
+df["Value"] = df["Value"].astype(float).round(3)
 
-out = Path('out')
-out.mkdir(exist_ok=True)
-df.to_csv(out/'rainfall.csv', index = False)
-
-# ## No scraper present so we have created this manually
-
-# +
-with open('info.json') as f:
-    info_json = json.load(f)
-csvw_mapping = CSVWMapping()
-csvw_mapping.set_mapping(info_json)
-csvw_mapping.set_csv(out/"rainfall.csv")
-csvw_mapping.set_dataset_uri(f"http://gss-data.org.uk/data/gss_data/climate-change/{info_json['id']}")
-csvw_mapping.write(out/'rainfall.csv-metadata.json')
-
-shutil.copy("rainfall.csv-metadata.trig", out/"rainfall.csv-metadata.trig")
-
-
+df.to_csv('observations.csv', index=False)
+catalog_metadata = CatalogMetadata(
+    title = "Annual mean rainfall with trends actual",
+    description = "Priority dataset for Climate Change Platform project."
+)
+catalog_metadata.to_json_file('catalog-metadata.json')
