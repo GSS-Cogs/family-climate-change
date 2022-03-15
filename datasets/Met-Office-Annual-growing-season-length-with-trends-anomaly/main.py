187705bf1ef87d89700cc788cc339def9b81b6de
bailed22 <david.bailey@gsscogs.uk>
2022-03-15 16:01:06 +0000

    change to csvcubed pipeline

--- datasets/Met-Office-Annual-growing-season-length-with-trends-anomaly/main.py
+++ datasets/Met-Office-Annual-growing-season-length-with-trends-anomaly/main.py
@@ -1,40 +1,18 @@
-import json 
 import pandas as pd 
 from gssutils import *
-from pathlib import Path
-import shutil
-from gssutils.csvw.mapping import CSVWMapping
+from csvcubed.models.cube.qb.catalog import CatalogMetadata
 
 df = pd.read_csv("raw.csv")
-#df.drop(columns='daycount', axis=1, inplace=True)
-
 df = pd.melt(df, id_vars=['Year'])
-df.rename(columns={
-                'variable': 'Geography',
-                'value': 'Value'
-                }, inplace=True)
-
-#df['Month'] = pd.to_datetime(df['Month'], dayfirst=True).dt.strftime('%Y-%m')
-
-#df['Geography'].replace({'ondon': 'london'}, inplace=True)
+df.rename(columns={'variable': 'Geography','value': 'Value'}, inplace=True)
 df['Geography'] = df['Geography'].apply(pathify)
-#df['Rainfall'] = df['Rainfall'].astype(str).astype(float).round(2)
-
-out = Path('out')
-out.mkdir(exist_ok=True)
-df.to_csv(out/'growing-season.csv', index = False)
-
-# ## No scraper present so we have created this manually
 
 # +
-with open('info.json') as f:
-    info_json = json.load(f)
-csvw_mapping = CSVWMapping()
-csvw_mapping.set_mapping(info_json)
-csvw_mapping.set_csv(out/"growing-season.csv")
-csvw_mapping.set_dataset_uri(f"http://gss-data.org.uk/data/gss_data/climate-change/{info_json['id']}")
-csvw_mapping.write(out/'growing-season.csv-metadata.json')
-
-shutil.copy("growing-season.csv-metadata.trig", out/"growing-season.csv-metadata.trig")
+df.to_csv('observations.csv', index=False)
+catalog_metadata = CatalogMetadata(
+    title = "Annual growing season length with trends anomaly",
+    description = "Priority dataset for Climate Change Platform project."
+)
+catalog_metadata.to_json_file('catalog-metadata.json')
 
 
