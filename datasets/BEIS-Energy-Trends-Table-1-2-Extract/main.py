818e5170a9ec94b2996f34452cc9a0dfcbf369eb
bailed22 <95625808+bailed22@users.noreply.github.com>
2022-03-15 15:51:21 +0000

    BEIS-Energy-Trends-Table-1-2-Extract (#378)

--- datasets/BEIS-Energy-Trends-Table-1-2-Extract/main.py
+++ datasets/BEIS-Energy-Trends-Table-1-2-Extract/main.py
@@ -1,41 +1,16 @@
 #!/usr/bin/env python
 # coding: utf-8
-
-# In[ ]:
-
-
-
 import pandas as pd
-from IPython.display import display
-
 from gssutils import *
-import json
-import re
 
-cubes = Cubes("info.json")
-info = json.load(open('info.json'))
+metadata = Scraper(seed='info.json')
 
+# +
 
-# In[ ]:
+distribution = metadata.distribution(title = lambda x: "Energy Trends total energy table(ODS) in x, metaType = (ODF Spreadsheet)",latest=True)
 
 
-
-scraper = Scraper(seed='info.json')
-scraper
-
-
-# In[ ]:
-
-
-
-dist = [x for x in scraper.distributions if '1.2' in x.title][0]
-dist
-
-
-# In[ ]:
-
-
-
+# +
 df = pd.read_csv("Energy Trends 1.2 IDP Extract.csv")
 
 df = df[:-5]
@@ -45,13 +20,6 @@ df = pd.melt(df, id_vars = ['Year'], var_name = 'Fuel', value_name = 'Value', ig
 
 df = df.rename(columns={'Year' : 'Period'})
 
-df
-
-
-# In[ ]:
-
-
-
 df['Period'] = df.apply(lambda x: 'year/' + str(x['Period'])[:-2], axis = 1)
 
 df['Region'] = 'K02000001'
@@ -60,8 +28,10 @@ df['Measure Type'] = 'Energy Consumption'
 
 df['Unit'] = 'Millions of Tonnes of Oil Equivalent'
 
-df = df[['Period', 'Region', 'Fuel', 'Value', 'Measure Type', 'Unit']]
+df = df[['Period', 'Region', 'Fuel', 'Measure Type', 'Unit', 'Value']]
 
+
+# +
 COLUMNS_TO_NOT_PATHIFY = ['Value', 'Period', 'Region', 'Marker']
 
 for col in df.columns.values.tolist():
@@ -72,48 +42,10 @@ for col in df.columns.values.tolist():
 	except Exception as err:
 		raise Exception('Failed to pathify column "{}".'.format(col)) from err
 
-df = df.replace({'Fuel' : {'-natural-gas' : 'natural-gas',
-                           'total' : 'all',
-                           'biogenergy-and-waste' : 'bioenergy-and-waste'}})
-
-df
+df = df.replace({'Fuel' : {'-natural-gas' : 'natural-gas','total' : 'all','biogenergy-and-waste' : 'bioenergy-and-waste'}})
+# -
 
 
-# In[ ]:
-
-
-
-scraper.dataset.title = dist.title
-scraper.dataset.family = 'climate-change'
-scraper.dataset.comment = """
-An overview of the trends in energy production and consumption in the United Kingdom for the previous quarter, focusing on: consumption, both primary and final by broad sector, including seasonally adjusted series
-"""
-scraper.dataset.contactPoint = "energy.stats@beis.gov.uk"
-
-csvName = 'observations'
-cubes.add_cube(scraper, df.drop_duplicates(), csvName)
-
-
-# In[ ]:
-
-
-cubes.output_all()
-
-
-# In[ ]:
-
-
-
-from IPython.core.display import HTML
-for col in df:
-    if col not in ['Value']:
-        df[col] = df[col].astype('category')
-        display(HTML(f"<h2>{col}</h2>"))
-        display(df[col].cat.categories)
-
-
-# In[ ]:
-
-
-
-
+df.to_csv('observations.csv', index=False)
+catalog_metadata = metadata.as_csvqb_catalog_metadata()
+catalog_metadata.to_json_file('catalog-metadata.json')
