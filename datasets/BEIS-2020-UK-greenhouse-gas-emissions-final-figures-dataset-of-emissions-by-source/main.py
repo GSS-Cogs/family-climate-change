185422641f50356a98ef2e42d2034380ab8da44b
bailed22 <95625808+bailed22@users.noreply.github.com>
2022-04-27 11:54:00 +0100

    BEIS-2020-UK-greenhouse-gas-emissions-final-figures-dataset-of-emissions-by-source (#409)

---
+++ datasets/BEIS-2020-UK-greenhouse-gas-emissions-final-figures-dataset-of-emissions-by-source/main.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+#
+# ## BEIS-2020-UK-greenhouse-gas-emissions-final-figures-dataset-of-emissions-by-source
+
+import pandas as pd
+from gssutils import *
+
+
+metadata = Scraper(seed="info.json")
+distribution = metadata.distribution(
+    mediaType="text/csv",
+    latest=True,
+    title=lambda x: "UK greenhouse gas emissions: final figures – dataset of emissions by source"
+    in x,
+)
+df = distribution.as_pandas(encoding="ISO-8859-1").fillna(" ")
+
+df.loc[
+    (df["National Communication Sub-sector"] == "(blank)"),
+    "National Communication Sub-sector",
+] = "Not Applicable"
+df.drop(columns="TerritoryName", axis=1, inplace=True)
+df.rename(columns={"ActivityName": "Activity Name"}, inplace=True)
+df.drop(
+    df.columns[df.columns.str.contains("Unnamed", case=False)], axis=1, inplace=True
+)
+
+# Fixing BEIS' use of slashes in some columns:
+df["National Communication Category"] = df[
+    "National Communication Category"
+].str.replace("/", "-")
+df["Source"] = df["Source"].str.replace("/", "-").str.replace("-+", "-", regex=True)
+
+df = pd.melt(
+    df,
+    id_vars=[
+        "GHG",
+        "GHG Grouped",
+        "IPCC Code",
+        "Year",
+        "National Communication Sector",
+        "National Communication Sub-sector",
+        "National Communication Category",
+        "Source",
+        "National Communication Fuel Group",
+        "National Communication Fuel",
+        "Activity Name",
+    ],
+    value_vars=["Emissions (MtCO2e, AR4 GWPs)", "Emissions (MtCO2e, AR5 GWPs)"],
+    var_name="Measure",
+    value_name="Value",
+)
+
+df['Value'] = pd.to_numeric(df['Value'], errors="raise", downcast="float")
+df["Value"] = df["Value"].astype(float).round(3)
+df["Measure"] = df["Measure"].str.replace("MtCO2e, ", "")
+df['Unit'] = 'millions of tonnes of carbon dioxide equivalent (mtco2e)'
+
+df = df[['GHG',
+         'GHG Grouped',
+         'IPCC Code',
+         'Year',
+         'National Communication Sector',
+         'National Communication Sub-sector',
+         'National Communication Category',
+         'Source',
+         'National Communication Fuel Group',
+         'National Communication Fuel',
+         'Activity Name',
+         'Measure',
+         'Unit',
+         'Value']]
+
+for col in df.columns.values.tolist()[4:-1]:
+    try:
+        df[col] = df[col].apply(pathify)
+    except Exception as err:
+        raise Exception('Failed to pathify column "{}".'.format(col)) from err
+
+df.to_csv("observations.csv", index=False)
+catalog_metadata = metadata.as_csvqb_catalog_metadata()
+catalog_metadata.to_json_file("catalog-metadata.json")
