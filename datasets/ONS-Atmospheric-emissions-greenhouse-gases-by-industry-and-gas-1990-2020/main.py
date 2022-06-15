0c88c6f9ed6e8b9f85d55aa63e4cbc6b939ac589
“osamede20” <osamede.asemota@gsscogs.uk>
2022-06-15 12:10:34 +0000

    correcting codes

--- datasets/ONS-Atmospheric-emissions-greenhouse-gases-by-industry-and-gas-1990-2020/main.py
+++ datasets/ONS-Atmospheric-emissions-greenhouse-gases-by-industry-and-gas-1990-2020/main.py
@@ -1,25 +1,20 @@
-# %%
 # ## ONS-Atmospheric-emissions-greenhouse-gases-by-industry-and-gas-1990-2020
 
+# +
 import json
 import pandas as pandas
 from gssutils import *
 
 info = json.load(open('info.json'))
 metadata = Scraper(seed="info.json")
-title = info['title']
-metadata.dataset.title = title
+metadata.dataset.title = info['title']
 
 distribution = metadata.distribution(
     mediaType="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", latest=True)
 
-metadata.dataset.title = title
-
-# %%
+# +
 # reterieve the id from info.json for URI's (use later)
-with open("info.json", "r") as read_file:
-    data = json.load(read_file)
-    title_id = data['id']
+title_id = info['id']
 
 def pathify_section_values(section):
     if 'Total' in section:
@@ -32,22 +27,21 @@ def pathify_section_values(section):
         return section
 
 
-# %%
+# -
 
 tabs = distribution.as_databaker()
 tidied_sheets = []
 for tab in tabs:
     if 'Contents' in tab.name:
         continue
-    remove_bottom_section = tab.excel_ref('A29').expand(DOWN).expand(RIGHT)
-    # remove_bottom_section = tab.filter('SIC(07 group').expand(DOWN).expand(RIGHT)
+    remove_bottom_section = tab.excel_ref('A26').expand(DOWN).expand(RIGHT)
+
     year = tab.excel_ref('D4').expand(RIGHT).is_not_blank()
-    section = tab.excel_ref('A5').expand(DOWN)
-    section_name = tab.excel_ref('C5').expand(DOWN)
+    section = tab.excel_ref('A5').expand(DOWN) - remove_bottom_section
+    section_name = tab.excel_ref('C5').expand(DOWN) - remove_bottom_section
     observations = year.fill(DOWN).is_not_blank() - remove_bottom_section
     measure_type = tab.excel_ref('AH3')
 
-    # Top part (includes 2020 provisional data for total ghg)
     dimensions = [
         HDim(section, 'Section breakdown', DIRECTLY, LEFT),  # will be dropped
         HDim(section_name, 'Industry Section Name',
@@ -60,19 +54,19 @@ for tab in tabs:
     #savepreviewhtml(tidy_sheet, fname = tab.name+ "Preview.html")
     table = tidy_sheet.topandas()
     table['Section'] = table.apply(lambda x: x['Industry Section Name'] if x['Section breakdown'] == '-'
-                                   else (x['Industry Section Name'] if x['Section breakdown'] == ''
-                                         else x['Section breakdown']), axis=1)
+                                   else x['Section breakdown'], axis=1)
     table['Section'] = table['Section'].apply(pathify_section_values)
     tidied_sheets.append(table)
 
     # Bottom part
-    section_name = tab.excel_ref('C31').expand(DOWN)
-    remove_top_section = tab.excel_ref('A28').expand(UP).expand(RIGHT)
-    remove_notes = tab.excel_ref('A165').expand(DOWN).expand(RIGHT)
+
+    # remove_top_section = tab.excel_ref('A27').expand(UP).expand(RIGHT)
+    remove_notes = tab.excel_ref('A164').expand(DOWN).expand(RIGHT)
+
     sic_group = tab.excel_ref('A31').expand(DOWN) - remove_notes
     section = tab.excel_ref('B31').expand(DOWN) - remove_notes
-    observations = year.fill(DOWN).is_not_blank() - \
-        remove_top_section - remove_notes
+    section_name = tab.excel_ref('C31').expand(DOWN) - remove_notes
+    observations = section_name.fill(RIGHT).is_not_blank()
 
     dimensions = [
         HDim(sic_group, 'SIC(07)Group', DIRECTLY, LEFT),
@@ -88,8 +82,7 @@ for tab in tabs:
     #savepreviewhtml(tidy_sheet, fname = tab.name+ "Preview.html")
     table = tidy_sheet.topandas()
     table['Section'] = table.apply(lambda x: x['Industy Section Name'] if x['SIC(07)Group'] == '-'
-                                   else (x['Industry Section Name'] if x['SIC(07)Group'] == ''
-                                         else x['SIC(07)Group']), axis=1)
+                                   else x['SIC(07)Group'], axis=1)
 
     table['Section'] = table['Section'].str.rstrip("0")
     table['Section'] = table['Section'].str.rstrip(".")
@@ -98,9 +91,7 @@ for tab in tabs:
     table['Section'] = table['Section'].apply(pathify)
     tidied_sheets.append(table)
 
-#
-
-# %%
+# +
 df = pd.concat(tidied_sheets, sort=True)
 df.rename(columns={'OBS': 'Value', 'DATAMARKER': 'Marker'}, inplace=True)
 df = df.replace(
@@ -115,19 +106,17 @@ sic = 'http://business.data.gov.uk/companies/def/sic-2007/'
 df['Section'] = df['Section'].map(
     lambda x: unique + x if '-' in x else (unique + x if 'total' in x else sic + x))
 
-df['Emission Type'] = df['Emission Type'].apply(pathify)
-df = df.replace({'Emission Type': {'total-ghg': 'ghg-total'}})
+# df['Emission Type'] = df['Emission Type'].apply(pathify)
+# df = df.replace({'Emission Type': {'total-ghg': 'ghg-total'}})
 df['Measure Type'] = df['Measure Type'].str.strip()
 df['Measure Type'] = df['Measure Type'].map(
     lambda x: 'Mass of air emissions of carbon dioxide equivalent' if 'carbon dioxide' in x else 'Mass of air emissions')
-df['Measure Type'] = df['Measure Type'].apply(pathify)
-# df['Units'] = 'thousand-tonnes'
+# df['Measure Type'] = df['Measure Type'].apply(pathify)
+
 # only need the following columns
 df = df[['Year', 'Section', 'Emission Type', 'Measure Type', 'Value']]
 # -
-# %%
 df = df.drop_duplicates()
-# %%
 df.to_csv('observations.csv', index=False)
 catalog_metadata = metadata.as_csvqb_catalog_metadata()
 catalog_metadata.to_json_file('catalog-metadata.json')
