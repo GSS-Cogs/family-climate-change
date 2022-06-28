6c8d6e6e2b44378bae9c97c05a51d924b5c7f5a5
Osamede Asemota <osamede.asemota@gsscogs.uk>
2022-06-28 12:42:19 +0100

    correcting script to use only industry total for 'GHG total' (#476)

--- datasets/ONS-Atmospheric-emissions-greenhouse-gases-by-industry-and-gas-1990-2020/main.py
+++ datasets/ONS-Atmospheric-emissions-greenhouse-gases-by-industry-and-gas-1990-2020/main.py
@@ -29,62 +29,88 @@ tidied_sheets = []
 for tab in tabs:
     if 'Contents' in tab.name:
         continue
-    remove_bottom_section = tab.excel_ref('A26').expand(DOWN).expand(RIGHT)
+    elif 'GHG total' in tab.name:
+        remove_bottom_section = tab.excel_ref('A26').expand(DOWN).expand(RIGHT)
 
-    year = tab.excel_ref('D4').expand(RIGHT).is_not_blank()
-    section = tab.excel_ref('A5').expand(DOWN) - remove_bottom_section
-    section_name = tab.excel_ref('C5').expand(DOWN) - remove_bottom_section
-    observations = year.fill(DOWN).is_not_blank() - remove_bottom_section
-    measure_type = tab.excel_ref('AH3')
+        year = tab.excel_ref('D4').expand(RIGHT).is_not_blank()
+        section = tab.excel_ref('A5').expand(DOWN) - remove_bottom_section
+        section_name = tab.excel_ref('C5').expand(DOWN) - remove_bottom_section
+        observations = year.fill(DOWN).is_not_blank() - remove_bottom_section
+        measure_type = tab.excel_ref('AH3')
 
-    dimensions = [
-        HDim(section, 'Section breakdown', DIRECTLY, LEFT),  # will be dropped
-        HDim(section_name, 'Industry Section Name',
-             DIRECTLY, LEFT),  # will be dropped
-        HDim(year, 'Year', DIRECTLY, ABOVE),
-        HDimConst('Emission Type', tab.name),
-        HDim(measure_type, 'Measure Type', CLOSEST, ABOVE),
-    ]
-    tidy_sheet = ConversionSegment(tab, dimensions, observations)
-    #savepreviewhtml(tidy_sheet, fname = tab.name+ "Preview.html")
-    table = tidy_sheet.topandas()
-    table['Section'] = table.apply(lambda x: x['Industry Section Name'] if x['Section breakdown'] == '-'
-                                   else x['Section breakdown'], axis=1)
-    table['Section'] = table['Section'].apply(pathify_section_values)
-    tidied_sheets.append(table)
+        dimensions = [
+            HDim(section, 'Section breakdown', DIRECTLY, LEFT),  # will be dropped
+            HDim(section_name, 'Industry Section Name',
+                DIRECTLY, LEFT),  # will be dropped
+            HDim(year, 'Year', DIRECTLY, ABOVE),
+            HDimConst('Emission Type', tab.name),
+            HDim(measure_type, 'Measure Type', CLOSEST, ABOVE),
+        ]
+        tidy_sheet = ConversionSegment(tab, dimensions, observations)
+        #savepreviewhtml(tidy_sheet, fname = tab.name+ "Preview.html")
+        table = tidy_sheet.topandas()
+        table['Section'] = table.apply(lambda x: x['Industry Section Name'] if x['Section breakdown'] == '-'
+                                    else x['Section breakdown'], axis=1)
+        table['Section'] = table['Section'].apply(pathify_section_values)
+        tidied_sheets.append(table)
 
-    # Bottom part
+    else:
+        remove_bottom_section = tab.excel_ref('A26').expand(DOWN).expand(RIGHT)
 
-    # remove_top_section = tab.excel_ref('A27').expand(UP).expand(RIGHT)
-    remove_notes = tab.excel_ref('A162').expand(DOWN).expand(RIGHT)
+        year = tab.excel_ref('D4').expand(RIGHT).is_not_blank()
+        section = tab.excel_ref('A5').expand(DOWN) - remove_bottom_section
+        section_name = tab.excel_ref('C5').expand(DOWN) - remove_bottom_section
+        observations = year.fill(DOWN).is_not_blank() - remove_bottom_section
+        measure_type = tab.excel_ref('AH3')
 
-    sic_group = tab.excel_ref('A31').expand(DOWN) - remove_notes
-    section = tab.excel_ref('B31').expand(DOWN) - remove_notes
-    section_name = tab.excel_ref('C31').expand(DOWN) - remove_notes
-    observations = section_name.fill(RIGHT).is_not_blank()
+        dimensions = [
+            HDim(section, 'Section breakdown', DIRECTLY, LEFT),  # will be dropped
+            HDim(section_name, 'Industry Section Name',
+                DIRECTLY, LEFT),  # will be dropped
+            HDim(year, 'Year', DIRECTLY, ABOVE),
+            HDimConst('Emission Type', tab.name),
+            HDim(measure_type, 'Measure Type', CLOSEST, ABOVE),
+        ]
+        tidy_sheet = ConversionSegment(tab, dimensions, observations)
+        #savepreviewhtml(tidy_sheet, fname = tab.name+ "Preview.html")
+        table = tidy_sheet.topandas()
+        table['Section'] = table.apply(lambda x: x['Industry Section Name'] if x['Section breakdown'] == '-'
+                                    else x['Section breakdown'], axis=1)
+        table['Section'] = table['Section'].apply(pathify_section_values)
+        tidied_sheets.append(table)
 
-    dimensions = [
-        HDim(sic_group, 'SIC(07)Group', DIRECTLY, LEFT),
-        HDim(section, 'Section breakdown', DIRECTLY, LEFT),  # will be dropped
-        HDim(section_name, 'Industry Section Name',
-             DIRECTLY, LEFT),  # will be dropped
-        HDim(year, 'Year', DIRECTLY, ABOVE),
-        HDim(measure_type, 'Measure Type', CLOSEST, ABOVE),
-        HDimConst('Emission Type', tab.name),
-    ]
+        # Bottom part
 
-    tidy_sheet = ConversionSegment(tab, dimensions, observations)
-    #savepreviewhtml(tidy_sheet, fname = tab.name+ "Preview.html")
-    table = tidy_sheet.topandas()
-    table['Section'] = table.apply(lambda x: x['Industy Section Name'] if x['SIC(07)Group'] == '-'
-                                   else x['SIC(07)Group'], axis=1)
+        # remove_top_section = tab.excel_ref('A27').expand(UP).expand(RIGHT)
+        remove_notes = tab.excel_ref('A162').expand(DOWN).expand(RIGHT)
 
-    table['Section'] = table['Section'].str.rstrip("0")
-    table['Section'] = table['Section'].str.rstrip(".")
-    table['Section'] = table['Section'].apply(lambda x: '{0:0>2}'.format(x))
-    table['Section'] = table['Section'].apply(pathify_section_values)
-    table['Section'] = table['Section'].apply(pathify)
-    tidied_sheets.append(table)
+        sic_group = tab.excel_ref('A31').expand(DOWN) - remove_notes
+        section = tab.excel_ref('B31').expand(DOWN) - remove_notes
+        section_name = tab.excel_ref('C31').expand(DOWN) - remove_notes
+        observations = section_name.fill(RIGHT).is_not_blank()
+
+        dimensions = [
+            HDim(sic_group, 'SIC(07)Group', DIRECTLY, LEFT),
+            HDim(section, 'Section breakdown', DIRECTLY, LEFT),  # will be dropped
+            HDim(section_name, 'Industry Section Name',
+                DIRECTLY, LEFT),  # will be dropped
+            HDim(year, 'Year', DIRECTLY, ABOVE),
+            HDim(measure_type, 'Measure Type', CLOSEST, ABOVE),
+            HDimConst('Emission Type', tab.name),
+        ]
+
+        tidy_sheet = ConversionSegment(tab, dimensions, observations)
+        #savepreviewhtml(tidy_sheet, fname = tab.name+ "Preview.html")
+        table = tidy_sheet.topandas()
+        table['Section'] = table.apply(lambda x: x['Industy Section Name'] if x['SIC(07)Group'] == '-'
+                                    else x['SIC(07)Group'], axis=1)
+
+        table['Section'] = table['Section'].str.rstrip("0")
+        table['Section'] = table['Section'].str.rstrip(".")
+        table['Section'] = table['Section'].apply(lambda x: '{0:0>2}'.format(x))
+        table['Section'] = table['Section'].apply(pathify_section_values)
+        table['Section'] = table['Section'].apply(pathify)
+        tidied_sheets.append(table)
 
 df = pd.concat(tidied_sheets, sort=True)
 df.rename(columns={'OBS': 'Value', 'DATAMARKER': 'Marker'}, inplace=True)
