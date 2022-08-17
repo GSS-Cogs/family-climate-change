be002ccb086179904713fdfffd6c9b38a2439c18
santhosh thangavel <santhoshthangavel@Santhoshs-MacBook-Pro.local>
2022-08-17 15:02:13 +0100

    transform of last two tabs

--- datasets/MHCLG-Table-NB1-domestic-Energy-Performance-Certificates-for-new-dwellings-by-energy-efficiency-rating/main.py
+++ datasets/MHCLG-Table-NB1-domestic-Energy-Performance-Certificates-for-new-dwellings-by-energy-efficiency-rating/main.py
@@ -47,10 +47,19 @@ multi_lists[1]
 df1 = pd.DataFrame(multi_lists[0])
 df1
 
+df1["location"] = "England and Wales"
+df1
+
 df2 = pd.DataFrame(multi_lists[1])
 df2
 
+df2["location"] = "http://statistics.data.gov.uk/id/statistical-geography/E92000001"
+df2
+
 df3 = pd.DataFrame(multi_lists[2])
+df3
+
+df3["location"] = "http://data.europa.eu/nuts/code/UKL"
 df3
 
 df = pd.concat([df1,df2, df3])
@@ -83,5 +92,55 @@ df.columns
 # +
 # efficiency_rating
 
-pd.melt(df, id_vars = ['Quarter', 'Number of Lodgements', 'Total Floor Area (m2)'], value_vars = ['A', 'B',
-       'C', 'D', 'E', 'F', 'G', 'Not Recorded'], ignore_index=False)
+frame1 = pd.melt(df, id_vars = ['Quarter', 'Number of Lodgements', 'Total Floor Area (m2)', 'location'], value_vars = ['A', 'B',
+       'C', 'D', 'E', 'F', 'G', 'Not Recorded'], var_name = "Efficieny Rating", ignore_index=False)
+# -
+
+frame1
+
+second_list_df = pd.read_excel(distribution.downloadURL, sheet_name = "NB1_By_Region", header = 3)
+second_list_df
+
+type(second_list_df)
+
+second_list_df.rename(columns = {"Region":"location"}, inplace = True)
+
+second_list_df
+
+# +
+# efficiency_rating
+
+frame2 = pd.melt(second_list_df, id_vars = ['Quarter', 'Number of Lodgements', 'Total Floor Area (m2)', 'location'], value_vars = ['A', 'B',
+       'C', 'D', 'E', 'F', 'G', 'Not Recorded'], var_name = "Efficieny Rating", ignore_index=False)
+# -
+
+frame2
+
+third_list_df = pd.read_excel(distribution.downloadURL, sheet_name = "NB1_By_LA", header = 3)
+third_list_df
+
+third_list_df.drop(["Local Authority"], axis = 1, inplace = True)
+
+third_list_df
+
+third_list_df.rename(columns = {"Local Authority Code":"location"}, inplace = True)
+
+third_list_df
+
+# +
+# efficiency_rating
+
+frame3 = pd.melt(third_list_df, id_vars = ['Quarter', 'Number of Lodgements', 'Total Floor Area (m2)', 'location'], value_vars = ['A', 'B',
+       'C', 'D', 'E', 'F', 'G', 'Not Recorded'], var_name = "Efficieny Rating", ignore_index=False)
+# -
+
+frame3
+
+frames = [frame1, frame2, frame3]
+
+tidy = pd.concat(frames).fillna('')
+
+tidy.rename(columns = {"Quarter":"period"}, inplace = True)
+tidy
+
+
