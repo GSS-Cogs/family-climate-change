4f1427e3dc5252969e951bca7b67b0f58578b5d4
santhosh thangavel <santhoshthangavel@Santhoshs-MacBook-Pro.local>
2022-08-18 17:49:08 +0100

    completed postprocessing

--- datasets/MHCLG-Table-NB1-domestic-Energy-Performance-Certificates-for-new-dwellings-by-energy-efficiency-rating/main.py
+++ datasets/MHCLG-Table-NB1-domestic-Energy-Performance-Certificates-for-new-dwellings-by-energy-efficiency-rating/main.py
@@ -20,6 +20,9 @@ import numpy as np
 metadata = Scraper(seed="info.json")
 distribution = metadata.distribution(title = lambda x: "Table NB1" in x)
 
+info = json.load(open('info.json'))
+title_id = info['id']
+
 distribution
 
 df = pd.read_excel(distribution.downloadURL, sheet_name = ["NB1", "NB1_England_Only", "NB1_Wales_Only"], header = 3)
@@ -198,10 +201,48 @@ tidy = tidy[~tidy["Period"].isin(["Total"])]
 
 tidy
 
-tidy["Period"] =  tidy["Period"].astype(str).apply(lambda x: "year/" + x[:4] if len(x) == 4 else "quarter/" + x[:4] + "-0" + x[5:6])
+tidy["Period"].unique()
+
+tidy["Period"] =  tidy["Period"].astype(str).apply(lambda x: "year/" + x[:4] if len(x) == 4 else "quarter/" + x[:4] + "-0" + x[5:6] if len(x) == 6 else '')
 
 tidy["Period"].unique()
 
 tidy
 
+tidy = tidy.replace({'Location': {
+    "East Midlands": "http://data.europa.eu/nuts/code/UKF",
+    "London": "http://data.europa.eu/nuts/code/UKI",
+    "North East": "http://data.europa.eu/nuts/code/UKC",
+    "North West": "http://data.europa.eu/nuts/code/UKD",
+    "South East": "http://data.europa.eu/nuts/code/UKJ",
+    "South West": "http://data.europa.eu/nuts/code/UKK",
+    "East of England": "http://data.europa.eu/nuts/code/UKH",
+    "West Midlands": "http://data.europa.eu/nuts/code/UKG",
+    "Yorkshire and The Humber": "http://data.europa.eu/nuts/code/UKE",
+    "Unknown": 'http://gss-data.org.uk/data/gss_data/climate-change/' +
+    title_id + '#concept/local-authority-code/unknown',
+    "England and Wales" : 'http://gss-data.org.uk/data/gss_data/climate-change/' +
+    title_id + '#concept/local-authority-code/england-wales'
+}})
 
+sic = 'http://statistics.data.gov.uk/id/statistical-geography/'
+tidy['location'] = tidy['location'].map(
+    lambda x: sic + x if 'E0' in x else (  sic + x if 'W0' in x else x))
+
+tidy = tidy.replace({'Efficieny Rating': {
+    "Not recorded": "Not Recorded",
+    "not-recorded": 'Not Recorded'
+    }})
+
+tidy['Measure Type'] = 'energy-performance-certificates'
+tidy['Unit'] = 'count'
+# tidy = tidy[['Period', 'Efficieny Rating', 'Location', 'Lodgements', 'Total Floor Area (m2)','Measure Type', 'Unit', 'Value']]
+
+tidy.columns
+
+tidy = tidy[['Period', 'Number of Lodgements', 'Total Floor Area (m2)', 'location',
+       'Efficieny Rating', 'value', 'Measure Type', 'Unit']]
+
+tidy.to_csv('observations.csv', index=False)
+catalog_metadata = metadata.as_csvqb_catalog_metadata()
+catalog_metadata.to_json_file('catalog-metadata.json')
