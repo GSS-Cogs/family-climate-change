feb5c1cf4fa0c15dd8594a70c65ee009e78503a0
Shannon Black <shannon1995black@gmail.com>
2022-10-04 15:24:56 +0100

    Updated to mapping : Publisher, theme, creator, dimensions, measures, units. Removed out folder as not required here. Fixed white spacing in wrangling.py file.

--- datasets/IEA-Energy-RD-E-Spend/wrangling.py
+++ datasets/IEA-Energy-RD-E-Spend/wrangling.py
@@ -3,6 +3,7 @@ import click
 import pandas as pd
 from pathlib import Path
 
+
 @click.command()
 @click.argument("input", type=click.Path(exists=True, path_type=Path))
 @click.option("--output", default=Path("./output.csv"), type=click.Path(path_type=Path))
@@ -10,9 +11,11 @@ def wrangle(input: Path(), output: Path()) -> None:
     df = pd.read_excel(input, skiprows=[0])
     df = pd.melt(frame=df, id_vars=[
                  'Country', 'Currency', 'Economic Indicators'], var_name='Year', value_name='Value')
-    df["Unit"] = "Million"
+    df['Economic Indicators'] = df['Economic Indicators'].str.rstrip()
+    df = df.replace({'Country': {"United Kingdom": "K02000001"}})
     df.to_csv(output, index=False)
     return
+
 
 if __name__ == "__main__":
     wrangle()
