25877c42f39625efcc70a18d0a0bbc254e554a57
“osamede20” <osamede.asemota@gsscogs.uk>
2022-10-10 12:48:37 +0000

    correcting bad entries on source data

--- datasets/DEFRA-Carbon-Footprint-Summary-Final-Demand-90-19/main.py
+++ datasets/DEFRA-Carbon-Footprint-Summary-Final-Demand-90-19/main.py
@@ -16,55 +16,61 @@ distribution = metadata.distribution(
 metadata.dataset.title = "Carbon Footprint - Summary Final Demand 90-19"
 metadata.dataset.comment = "Greenhouse gas emissions by; region, households, consumables, services"
 
-tabs = {tab.name: tab for tab in distribution.as_databaker()}
+tabs = distribution.as_databaker()
 
 # +
 tidied_sheets = []
-for name, tab in tabs.items():
-    
-    if 'Summary_final_demand_90-19' not in name:
-        continue
 
-    # Greenhouse Gas emissions - GHG - Ktonnes CO2e	
-    unwanted_cell = tab.excel_ref("A34").expand(DOWN).expand(RIGHT)
-    period = tab.excel_ref("B4").expand(DOWN).is_not_blank().is_not_whitespace() - unwanted_cell
-    final_demand = tab.excel_ref("C2").expand(RIGHT)
-    final_demand_breakdown = tab.excel_ref("C3").expand(RIGHT).is_not_blank()
-    observations = tab.excel_ref("C4").expand(DOWN).expand(RIGHT).is_not_blank() - unwanted_cell
-    measure = 'Greenhouse gas emissions'
-    unit = 'kt CO2e'
-    
-    dimensions = [
-        HDim(period,'Period',DIRECTLY,LEFT),
-        HDim(final_demand, 'Final Demand Code',DIRECTLY, ABOVE),
-        HDim(final_demand_breakdown, 'Final Demand Breakdown',DIRECTLY,ABOVE),
-        HDimConst("Measure", measure),
-        HDimConst("Unit", unit)
-    ]
-    tidy_sheet = ConversionSegment(tab, dimensions, observations) 
-    tidied_sheets.append(tidy_sheet.topandas())
+for tab in tabs:    
+    if tab.name == 'Summary_final_demand_90-19':
 
-# from Carbon Dioxide Emission
-    period = tab.excel_ref("B38").expand(DOWN).is_not_blank().is_not_whitespace()
-    final_demand = tab.excel_ref("C36").expand(RIGHT)
-    final_demand_breakdown = tab.excel_ref("C37").expand(RIGHT)
-    observations = tab.excel_ref("C38").expand(DOWN).expand(RIGHT).is_not_blank().is_not_whitespace()
-    measure = 'Carbon dioxide emissions'
-    unit = 'kt CO2'
+        # Greenhouse Gas emissions - GHG - Ktonnes CO2e	
+        unwanted_cell = tab.excel_ref("A34").expand(DOWN).expand(RIGHT)
+        period = tab.excel_ref("B4").expand(DOWN).is_not_blank().is_not_whitespace() - unwanted_cell
+        final_demand = tab.excel_ref("C2").expand(RIGHT)
+        final_demand_breakdown = tab.excel_ref("C3").expand(RIGHT).is_not_blank()
+        observations = tab.excel_ref("C4").expand(DOWN).expand(RIGHT).is_not_blank() - unwanted_cell
+        measure = 'Greenhouse gas emissions'
+        unit = 'kt CO2e'
+        
+        dimensions = [
+            HDim(period,'Period',DIRECTLY,LEFT),
+            HDim(final_demand, 'Final Demand Code',DIRECTLY, ABOVE),
+            HDim(final_demand_breakdown, 'Final Demand Breakdown',DIRECTLY,ABOVE),
+            HDimConst("Measure", measure),
+            HDimConst("Unit", unit)
+        ]
+        tidy_sheet = ConversionSegment(tab, dimensions, observations) 
+        tidied_sheets.append(tidy_sheet.topandas())
 
-    dimensions = [
-        HDim(period,'Period',DIRECTLY,LEFT),
-        HDim(final_demand, 'Final Demand Code',DIRECTLY, ABOVE),
-        HDim(final_demand_breakdown, 'Final Demand Breakdown',DIRECTLY,ABOVE),
-        HDimConst("Measure", measure),
-        HDimConst("Unit", unit)
-    ]
-    tidy_sheet = ConversionSegment(tab, dimensions, observations) 
-    tidied_sheets.append(tidy_sheet.topandas())
-    # savepreviewhtml(tidy_sheet,fname=tab.name + "Preview.html")
+        # from Carbon Dioxide Emission
+        period = tab.excel_ref("B38").expand(DOWN).is_not_blank().is_not_whitespace()
+        final_demand = tab.excel_ref("C36").expand(RIGHT)
+        final_demand_breakdown = tab.excel_ref("C37").expand(RIGHT)
+        observations = tab.excel_ref("C38").expand(DOWN).expand(RIGHT).is_not_blank().is_not_whitespace()
+        measure = 'Carbon dioxide emissions'
+        unit = 'kt CO2'
+
+        dimensions = [
+            HDim(period,'Period',DIRECTLY,LEFT),
+            HDim(final_demand, 'Final Demand Code',DIRECTLY, ABOVE),
+            HDim(final_demand_breakdown, 'Final Demand Breakdown',DIRECTLY,ABOVE),
+            HDimConst("Measure", measure),
+            HDimConst("Unit", unit)
+        ]
+        tidy_sheet = ConversionSegment(tab, dimensions, observations) 
+        tidied_sheets.append(tidy_sheet.topandas())
+        # savepreviewhtml(tidy_sheet,fname=tab.name + "Preview.html")
 # -
 
 df = pd.concat(tidied_sheets, sort=True)
+
+df['Final Demand Breakdown'].unique()
+
+df = df.replace({'Final Demand Breakdown': {
+                                   'Non-profitinstitutions servinghouseholds' : 'Non-profit institutions serving households',
+                                   'Gross fixedcapitalformation' : 'Gross fixed capital formation',
+                                   '' : 'Total'}}) 
 
 # +
 df['Final Demand Code'] = df.apply(lambda x: 'UK FD 1' if x['Final Demand Breakdown'] == 'Households direct' else x['Final Demand Code'], axis=1)
