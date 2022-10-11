c62920c4bfe9954c9ef17fba7c32f81341c4f77a
bailed22 <david.bailey@gsscogs.uk>
2022-10-11 17:32:46 +0000

    transformed main.py to new legacy pipeline

--- datasets/DEFRA-E8-Efficient-use-of-water-20-21/main.py
+++ datasets/DEFRA-E8-Efficient-use-of-water-20-21/main.py
@@ -1,22 +1,18 @@
-#%%
-import json 
+#              DEFRA-E8-EFFICIENT-USE-OF-WATER-20-21"
+
 import pandas as pd 
 from gssutils import *
-from csvcubed.models.cube.qb.catalog import CatalogMetadata # need this extra import when working with local CSV files
-
+from csvcubed.models.cube.qb.catalog import CatalogMetadata
 
 df = pd.read_csv("raw.csv")
-#%%
+df['Year'] = df['Year'].str.replace(r'-20', r'-')
+df = pd.melt(df, id_vars=['Year'])
+df.rename(columns={'variable': 'Geography','value': 'Value'}, inplace=True)
 
-df.rename(columns={
-                "Year": "Period",
-                "England": "Value"
-                }, inplace=True)
-
-#create out directory for all output files
-df.to_csv('observations.csv', index=False) #defra_e8_efficient_use_of_water_20_21-
+df.to_csv('observations.csv', index=False) 
 catalog_metadata = CatalogMetadata(
     title = "E8: Efficient use of water 20 - 21",
-    description = "Per capita water consumption (Litres/person/day)."
+    summary = "",
+    description = "Summary of geographical water consumption in England Only."
 )
 catalog_metadata.to_json_file('catalog-metadata.json')
