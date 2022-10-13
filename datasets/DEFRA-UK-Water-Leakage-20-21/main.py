2773bc44a67e407b95c449a0e14d3e7c7e0126c9
bailed22 <david.bailey@gsscogs.uk>
2022-10-13 15:10:19 +0000

    using a codlist for period

--- datasets/DEFRA-UK-Water-Leakage-20-21/main.py
+++ datasets/DEFRA-UK-Water-Leakage-20-21/main.py
@@ -2,17 +2,25 @@
 
 import pandas as pd 
 from gssutils import *
-from csvcubed.models.cube.qb.catalog import CatalogMetadata
 
-df = pd.read_csv("raw.csv")
+metadata = Scraper(seed='info.json')
+metadata.select_dataset(title = lambda x: 'E8: Efficient use of water' in x)
 
-df['Year'] = df['Year'].str.replace('-20', '-')
-df = pd.melt(df, id_vars=['Year'])
-df.rename(columns={'variable': 'Geography','value': 'Value'}, inplace=True)
+metadata.dataset.family = 'climate-change'
+metadata.dataset.title = "UK Water Leakage 20 - 21"
+
+distribution = metadata.distribution(mediaType='text/csv', latest=True)
+df = distribution.as_pandas()
+
+indexNames = df[df['Series'] == 'E8b'].index
+df.drop(indexNames, inplace=True)
+df.drop(columns=['Series'], inplace=True)
+
+df['Year'] = df['Year'].str.replace(r'-20', r'-') 
+df = df.rename(columns={'Year' : 'Period'})
+df['Value'] = pd.to_numeric(df['Value'], downcast='float')
+df['Value'] = df['Value'].astype(str).astype(float).round(2)
 
 df.to_csv('observations.csv', index=False)
-catalog_metadata = CatalogMetadata(
-    title = "UK Water Leakage 20 - 21",
-    description = "Summary of geographical water leaakage in England Only"
-)
+catalog_metadata = metadata.as_csvqb_catalog_metadata()
 catalog_metadata.to_json_file('catalog-metadata.json')
