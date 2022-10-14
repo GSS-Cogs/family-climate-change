89c83bdddfd190b3bef9c4e7a105e59e954fd9cc
Shannon Black <shannon1995black@gmail.com>
2022-10-14 09:43:22 +0100

    fix codes for geo.

--- datasets/Forestry-Research-Forestry-Statistics-2022-Woodland-Area/main.py
+++ datasets/Forestry-Research-Forestry-Statistics-2022-Woodland-Area/main.py
@@ -10,7 +10,7 @@ df = pd.read_excel("Ch1_Woodland_FS2022.xlsx",
 df = pd.melt(frame=df, id_vars=['Year'],
              var_name='Country', value_name='Value')
 df['Country'] = df['Country'].str.split(" \n").str[0]
-df = df.replace({'Country': {"UK": "E92000001", "Northern Ireland": "N92000002",
+df = df.replace({'Country': {"UK": "K02000001", "Northern Ireland": "N92000002",
                              "Scotland": "S92000003", "Wales": "W92000004", "England": "E92000001"}})
 
 df.to_csv("observations.csv", index=False)
