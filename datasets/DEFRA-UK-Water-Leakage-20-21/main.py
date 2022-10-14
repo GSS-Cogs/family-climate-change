fe201eb47565deb06112a2a84faf4296219e9bad
bailed22 <david.bailey@gsscogs.uk>
2022-10-14 11:31:10 +0000

    adding government year

--- datasets/DEFRA-UK-Water-Leakage-20-21/main.py
+++ datasets/DEFRA-UK-Water-Leakage-20-21/main.py
@@ -8,6 +8,9 @@ metadata.select_dataset(title = lambda x: 'E8: Efficient use of water' in x)
 
 metadata.dataset.family = 'climate-change'
 metadata.dataset.title = "UK Water Leakage 20 - 21"
+metadata.dataset.description = '''
+Climate change and a growing population will put increasing pressure on our water supplies. /n Ambitious reductions in water leakage have a significant role in maintaining /n secure supplies and protecting the environment. This indicator shows changes in the efficient /n use of water, focussing on leakage. Leakage of water in England are existing metrics reported to The Water Services /n Regulation Authority (Ofwat) and the Environment Agency.
+'''
 
 distribution = metadata.distribution(mediaType='text/csv', latest=True)
 df = distribution.as_pandas()
@@ -16,7 +19,8 @@ indexNames = df[df['Series'] == 'E8b'].index
 df.drop(indexNames, inplace=True)
 df.drop(columns=['Series'], inplace=True)
 
-df['Year'] = df['Year'].str.replace(r'-20', r'-') 
+df['Year'] = df['Year'].str.replace(r'-20', r'-')
+df['Year'] = df.apply(lambda x: 'government-year/' + x['Year'], axis = 1) 
 df = df.rename(columns={'Year' : 'Period'})
 df['Value'] = pd.to_numeric(df['Value'], downcast='float')
 df['Value'] = df['Value'].astype(str).astype(float).round(2)
