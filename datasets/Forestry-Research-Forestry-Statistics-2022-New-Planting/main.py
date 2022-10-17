30b5bfc087d4acf0785a37f6c9cdaa7b06d42549
Shannon Black <shannon1995black@gmail.com>
2022-10-17 11:54:22 +0100

    Transformation and mapping for #530.

---
+++ datasets/Forestry-Research-Forestry-Statistics-2022-New-Planting/main.py
@@ -0,0 +1,36 @@
+# %%
+import pandas as pandas
+from gssutils import *
+import pandas as pd
+from csvcubed.models.cube.qb.catalog import CatalogMetadata
+
+df = pd.read_excel("NPRS-machinereadable-16jun22.ods", sheet_name='C11', skiprows=4,  usecols=[
+                   "Year", "Broadleaves Total (thousand ha)", "Conifers Total (thousand ha)", "United Kingdom Total (thousand ha) [note 3]"], engine="odf")
+
+df = pd.melt(frame=df, id_vars=['Year'],
+             var_name='Measure Type', value_name='Value').dropna()
+df['Marker'] = df['Year'].str.split(" ").str[1]
+# [r] = revision, [p] = provisional, [z] = not applicable
+df = df.replace({'Marker': {"[r]": "revision", "[p]": "provisional",
+                            "[z]": "not-applicable"}})
+df['Year'] = df['Year'].astype('str')     
+df['Year'] = df['Year'].str[:4]
+df['Measure Type'] = df['Measure Type'].str.replace(r"\(.*?\)", "", regex=True).str.rstrip().str.strip("[note 3]").str.strip("United Kingdom ")
+df['Unit'] = 'thousand-hectares' 
+
+# %%
+df.to_csv("observations.csv", index=False)
+# %%
+# Total : Includes woodland formed by natural colonisation (where known).
+catalog_metadata = CatalogMetadata(
+    title="Forestry Statistics 2022 New Planting",
+    summary="New planting in the United Kingdom, 1971 - 2022",
+    publisher_uri="https://www.gov.uk/government/organisations/forest-research",
+    theme_uris=[
+        "https://uksa.statisticsauthority.gov.uk/themes/agriculture-energy-environment"],
+    landing_page_uris=[
+        "https://www.forestresearch.gov.uk/tools-and-resources/statistics/data-downloads/"],
+    #dataset_issue="2022-09-29",
+)
+
+catalog_metadata.to_json_file('catalog-metadata.json')
