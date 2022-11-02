70519d444e7e2d2e74054606698227e0642cfbdc
bailed22 <david.bailey@gsscogs.uk>
2022-11-02 13:06:45 +0000

    correction to metadata description

--- datasets/Met-Office-Regional-average-climate-observations-UK-annual-rainfall-2021/main.py
+++ datasets/Met-Office-Regional-average-climate-observations-UK-annual-rainfall-2021/main.py
@@ -1,17 +1,13 @@
 import pandas as pd 
 from gssutils import *
 from csvcubed.models.cube.qb.catalog import CatalogMetadata
-from datetime import datetime
 
-df = pd.read_csv("raw.csv")
-
-df['Year'] = df['Year'].astype(str).astype(int)
+df = pd.read_csv("regional-average-climate-observations-uk-annual-rainfall.csv")
 
 df.to_csv('observations.csv', index=False)
 catalog_metadata = CatalogMetadata(
-    title = "Regional average climate observations uk annual rainfall 2021",
+    title = "Regional annual average rainfall with trends 1836 - 2021",
     creator_uri = "https://www.gov.uk/government/organisations/the-meteorological-office",
-    publisher_uri = "https://www.gov.uk/government/organisations/met-office",
-    description = "Priority dataset for Climate Change Platform project."
+    publisher_uri = "https://www.gov.uk/government/organisations/met-office"
 )
 catalog_metadata.to_json_file('catalog-metadata.json')
