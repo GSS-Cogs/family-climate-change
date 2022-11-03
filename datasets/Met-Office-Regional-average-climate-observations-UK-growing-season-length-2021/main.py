f61c259d59d52b5fdf40a7cee8c77ed7388ff79d
Rich M <richard.p.morgan@ons.gov.uk>
2022-11-03 16:33:48 +0000

    removed landing page from catalgoue metadata

--- datasets/Met-Office-Regional-average-climate-observations-UK-growing-season-length-2021/main.py
+++ datasets/Met-Office-Regional-average-climate-observations-UK-growing-season-length-2021/main.py
@@ -42,8 +42,6 @@ catalog_metadata = CatalogMetadata(
     title="Regional average climate observations UK growing season length 2021",
     summary="Regional average climate observations UK growing season length, 1960 - 2021",
     publisher_uri="https://www.gov.uk/government/organisations/met-office",
-    landing_page_uris=[
-        "https://www.ons.gov.uk/economy/grossdomesticproductgdp/datasets/quarterlycountryandregionalgdp"],
     # dataset_issue="2022-09-29",
 )
 catalog_metadata.to_json_file(out/'catalog-metadata.json')
