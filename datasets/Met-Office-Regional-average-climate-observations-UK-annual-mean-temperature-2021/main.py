8cecb85e990605bed06792a69ecaf677e3375ce5
bailed22 <david.bailey@gsscogs.uk>
2022-11-03 22:26:27 +0000

    update to geography

--- datasets/Met-Office-Regional-average-climate-observations-UK-annual-mean-temperature-2021/main.py
+++ datasets/Met-Office-Regional-average-climate-observations-UK-annual-mean-temperature-2021/main.py
@@ -4,10 +4,19 @@ from csvcubed.models.cube.qb.catalog import CatalogMetadata
 
 df = pd.read_csv("regional-average-climate-observations-uk-annual-mean-temperature.csv")
 
+df['Measure Type'] = df.apply(
+    lambda x: 'Rainfall (Trend)' if 'trend' in x['Geography'] else 'Rainfall', axis=1)
+
+df['Geography'] = df['Geography'].apply(lambda x: 'K02000001' if 'uk' in x
+                                        else ('N92000002' if 'northern-ireland' in x
+                                              else ('E92000001' if 'england' in x
+                                                    else ('S92000003' if 'scotland' in x
+                                                          else ('W92000004' if 'wales' in x else x)))))
+
 df.to_csv('observations.csv', index=False)
 catalog_metadata = CatalogMetadata(
-    title = "Regional annual average mean temperature with trends 1884 - 2021",
-    creator_uri = "https://www.gov.uk/government/organisations/the-meteorological-office",
-    publisher_uri = "https://www.gov.uk/government/organisations/met-office"
+    title="Regional annual average mean temperature with trends 1884 - 2021",
+    creator_uri="https://www.gov.uk/government/organisations/the-meteorological-office",
+    publisher_uri="https://www.gov.uk/government/organisations/met-office"
 )
 catalog_metadata.to_json_file('catalog-metadata.json')
