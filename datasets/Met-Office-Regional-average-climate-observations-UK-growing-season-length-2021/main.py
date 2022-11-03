156c5fd4e0eb3792de19b89f65d583b1ce0f892d
Rich M <richard.p.morgan@ons.gov.uk>
2022-11-03 14:05:29 +0000

    replaced some geography values with code

--- datasets/Met-Office-Regional-average-climate-observations-UK-growing-season-length-2021/main.py
+++ datasets/Met-Office-Regional-average-climate-observations-UK-growing-season-length-2021/main.py
@@ -9,7 +9,23 @@ df = pd.read_csv("regional-average-climate-observations-uk-growing-season-length
 
 ##rename columns
 #df.rename(columns={'Year' : 'Period'}, inplace=True)
-
+#%%
+#replace geography names with codes. Look for codes here: https://statistics.data.gov.uk/home 
+df = df.replace({'Geography' : {
+    "uk":"K02000001"
+    ,"england":"E92000001"
+    ,"wales":"W08000001" # this did come from W08 European Electoral Region so check with SB if suitable
+    ,"scotland":"S04000001"
+    ,"northern-ireland":"N07000001"
+    ## waiting from feedback from SB on how to handle trends
+    #,"uk-trend":"tbc"
+    #,"england-trend":"tbc"
+    #,"wales-trend":"tbc"
+    #,"scotland-trend":"tbc"
+    #,"northern-ireland-trend":"tbc"
+    }}
+)
+#%%
 #make output directory
 out = Path('out')
 out.mkdir(exist_ok=True)
@@ -28,5 +44,5 @@ catalog_metadata = CatalogMetadata(
         "https://www.ons.gov.uk/economy/grossdomesticproductgdp/datasets/quarterlycountryandregionalgdp"],
     # dataset_issue="2022-09-29",
 )
-catalog_metadata.to_json_file('catalog-metadata.json')
+catalog_metadata.to_json_file(out/'catalog-metadata.json')
 
