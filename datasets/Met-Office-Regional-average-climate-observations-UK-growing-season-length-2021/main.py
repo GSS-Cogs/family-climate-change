8b33b7bfe2dc1d4837522f580e3bdc3d0305ec21
Rich M <richard.p.morgan@ons.gov.uk>
2022-11-03 13:27:11 +0000

    added in newer method of creating catalog_metadata manually

--- datasets/Met-Office-Regional-average-climate-observations-UK-growing-season-length-2021/main.py
+++ datasets/Met-Office-Regional-average-climate-observations-UK-growing-season-length-2021/main.py
@@ -3,13 +3,12 @@ import json
 import pandas as pd 
 from gssutils import *
 from pathlib import Path
-import shutil
-from gssutils.csvw.mapping import CSVWMapping
+from csvcubed.models.cube.qb.catalog import CatalogMetadata
 
 df = pd.read_csv("regional-average-climate-observations-uk-growing-season-length.csv")
 
-#rename columns
-df.rename(columns={'Year' : 'Period'}, inplace=True)
+##rename columns
+#df.rename(columns={'Year' : 'Period'}, inplace=True)
 
 #make output directory
 out = Path('out')
@@ -19,15 +18,15 @@ out.mkdir(exist_ok=True)
 df.to_csv(out/'regional-average-climate-observations-uk-growing-season-length.csv', index=False)
 
 # ## No scraper present so we have create catalogue metadata manually
-with open('info.json') as f:
-    info_json = json.load(f)
-csvw_mapping = CSVWMapping()
-csvw_mapping.set_mapping(info_json)
-csvw_mapping.set_csv(out/'regional-average-climate-observations-uk-growing-season-length.csv')
-csvw_mapping.set_dataset_uri(f"http://gss-data.org.uk/data/gss_data/climate-change/{info_json['id']}")
-csvw_mapping.write(out/'regional-average-climate-observations-uk-growing-season-length.csv-metadata.json')
-
-shutil.copy("regional-average-climate-observations-uk-growing-season-length.csv-metadata.trig", out/"regional-average-climate-observations-uk-growing-season-length.csv-metadata.trig")
-
-
+catalog_metadata = CatalogMetadata(
+    title="Regional average climate observations UK growing season length 2021",
+    summary="Regional average climate observations UK growing season length, 1960 - 2021",
+    publisher_uri="https://www.gov.uk/government/organisations/met-office",
+    theme_uris=[
+        "https://uksa.statisticsauthority.gov.uk/themes/agriculture-energy-environment"],
+    landing_page_uris=[
+        "https://www.ons.gov.uk/economy/grossdomesticproductgdp/datasets/quarterlycountryandregionalgdp"],
+    # dataset_issue="2022-09-29",
+)
+catalog_metadata.to_json_file('catalog-metadata.json')
 
