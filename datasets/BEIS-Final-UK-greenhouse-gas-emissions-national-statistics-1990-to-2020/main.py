e790ed83490f1992cee69cd66862506c228a1605
Osamede Asemota <osamede.asemota@gsscogs.uk>
2022-12-12 16:39:41 +0000

    BEIS-Final-UK-greenhouse-gas-emissions-national-statistics-1990-to-2020 (#558)

--- datasets/BEIS-Final-UK-greenhouse-gas-emissions-national-statistics-1990-to-2020/main.py
+++ datasets/BEIS-Final-UK-greenhouse-gas-emissions-national-statistics-1990-to-2020/main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
-# ### BEIS-Final-UK-greenhouse-gas-emissions-national-statistics-1990-to-2020
+# +
+# ## BEIS-Final-UK-greenhouse-gas-emissions-national-statistics-1990-to-2020
 
 # +
 import json
@@ -22,22 +23,25 @@ tabs = [
 ]
 tidied_sheets = []
 for tab in tabs:
-    
-    if tab.name in ["1_3", "1_4", "1_5", "1_6"]:
+    if tab.name in [ "1_2", "1_3", "1_4", "1_5", "1_6"]:
         cell = tab.filter("NC Sector")
         period = cell.shift(RIGHT).fill(RIGHT).is_not_blank().is_not_whitespace()
-        stop_cell = tab.filter("Grand Total").expand(RIGHT).expand(DOWN)
-        nc_category = tab.filter("NC Category").fill(DOWN) - stop_cell
+        unit = "Million of tonnes of carbon dioxide equivalent (MtCO2e)"
+        stop_cell = tab.filter("Grand Total")
+        nc_category = tab.filter("NC Category").fill(DOWN)
         nc_sector = nc_category.is_blank().shift(LEFT)
-        
-        if tab.name == "1_3":
+        nc_sub_sector = nc_category.shift(LEFT).is_not_blank()
+
+        if tab.name == "1_2":
+            gas = "Total Greenhouse Gases"
+        elif tab.name == "1_3":
             gas = "Carbon Dioxide CO2"
         elif tab.name == "1_4":
             gas = "Methane CH4"
         elif tab.name == "1_5":
             gas = "Nitrous Oxide N2O"
         elif tab.name == "1_6":
-            gas = "Fluorinated Gases (F Gases)"
+            gas = "Total Fluorinated Gases"
 
         observations = period.waffle(nc_category).is_not_blank().is_not_whitespace()
         
@@ -55,55 +59,146 @@ for tab in tabs:
         tidied_sheets.append(df)
 df = pd.concat(tidied_sheets, ignore_index=True)
 
+    elif tab.name == "3_1":
+        cell = tab.filter("Geographic coverage")
+
+        period = cell.shift(2).fill(RIGHT).is_not_whitespace()
+        gas = cell.shift(2).fill(DOWN).is_not_whitespace()
+        inclusions = cell.shift(1).fill(DOWN).is_not_whitespace()
+
+        geographic_coverage = cell.fill(DOWN).is_not_whitespace()
+
+        observations = period.waffle(gas)
+        unit = "Million of tonnes of carbon dioxide equivalent (MtCO2e)"
+
+        dimensions = [
+            HDim(period, "Period", DIRECTLY, ABOVE),
+            HDim(gas, "Gas", DIRECTLY, LEFT),
+            HDim(inclusions, "Inclusions-Exclusions", CLOSEST, ABOVE),
+            HDim(geographic_coverage, "Geographic Coverage", CLOSEST, ABOVE),
+            HDimConst("Unit", unit),
+        ]
+        tidy_sheet = ConversionSegment(tab, dimensions, observations)
+        df = tidy_sheet.topandas()
+        tidied_sheets.append(df)
 # +
+df = pd.concat(tidied_sheets, sort=False).fillna("")
+
 df.rename(
     columns={
-        "OBS": "Value","DATAMARKER": "Marker",
-        "Inclusions-Exclusions": "Breakdown",
-        },inplace=True)
+        "OBS": "Value","DATAMARKER": "Marker","Inclusions-Exclusions": "Breakdown",},inplace=True)
 
 df["Value"] = pd.to_numeric(df["Value"], errors="raise", downcast="float")
 df["Value"] = df["Value"].astype(float).round(3)
 df["Period"] = df["Period"].astype(float).astype(int)
 df['Period'] = 'year/' + df['Period'].astype(str)
 
-# # Fix missing sub-sector
-# combustion_categories = ["Stationary and mobile combustion", "Incidental lubricant combustion in engines - agriculture"]
-# df.loc[df["NC Category"].isin(combustion_categories), "NC Sub-sector"] = "Combustion"
+# df["NC Sub Sector"] = df.apply(
+#     lambda x: "All" if x["NC Sub Sector"] == x["NC Sector"] else x["NC Sub Sector"],
+#     axis=1,
+# )
+
+# Fix missing sub-sector
+combustion_categories = ["Stationary and mobile combustion", "Incidental lubricant combustion in engines - agriculture"]
+df.loc[df["NC Category"].isin(combustion_categories), "NC Sub Sector"] = "Combustion"
+
+def sector(row):
+    if row["NC Category"] == "":
+        if row["NC Sector"] == "":
+            return("Grand Total")
+        else:
+            return(row["NC Sector"])
+    else:
+        return(row["NC Category"])
+df["Sector"] =  df.apply(sector, axis=1)
  
-df["NC Sector"] = df["NC Sector"].str.replace("/", "-")
-df["NC Category"] = df["NC Category"].str.replace("/", "-")
-# -
+df["Sector"] = df["Sector"].str.replace("/", "-")
+
+assert not df['Sector'].isna().any()
+assert not (df['Sector'] == "").any()
+
+badInheritance = [
+    "Aviation between UK and Crown Dependencies",
+    "Shipping between UK and Crown Dependencies",
+    "Aviation between the Crown Dependencies and Overseas Territories",
+    "Aviation between UK and Overseas Territories",
+    "Shipping between UK and Overseas Territories"
+]
+df["Geographic Coverage"] = df.apply(
+    lambda x: "" if x["Geographic Coverage"] in badInheritance else x["Geographic Coverage"],
+    axis=1
+)
+
+df["Breakdown"] = df.apply(
+    lambda x: "" if x["Geographic Coverage"] in badInheritance else x["Breakdown"],
+    axis=1,
+)
+df["Breakdown"] = df.apply(
+    lambda x: "None"
+    if "" in x["Breakdown"] and x["Geographic Coverage"] == "United Kingdom"
+    else x["Breakdown"],
+    axis=1,
+)
+df["Breakdown"] = df.apply(
+    lambda x: "None" if x["Geographic Coverage"] in badInheritance else x["Breakdown"],
+    axis=1,
+)
+
 df = df.replace(
     {
-        "Geographic Coverage": {"United Kingdom only": "United Kingdom"}
+        "Geographic Coverage": {"United Kingdom only": "United Kingdom"},
     }
 )
 
-df = df.replace({'Gas' : {'Nitrous Oxide N2O' : 'Nitrous oxide (N2O)',
-                          'Methane CH4' : 'Methane (CH4)',
-                          'Carbon Dioxide CO2' :'Carbon dioxide (CO2)'}})
+indexNames = df[df["Breakdown"] == "Net emissions/removals from LULUCF"].index
+df.drop(indexNames, inplace=True)
 
-# +
-# df.rename(
-#     columns={"NC Category": "NC Sub Sector"},inplace=True)
-# -
+indexNames = df[df["Gas"] == "Total"].index
+df.drop(indexNames, inplace=True)
 
+df = df.replace(
+    {
+        "Geographic Coverage": {"": "United Kingdom"},
+        "Breakdown": {"": "All"},
+    }
+)
+
+COLUMNS_TO_NOT_PATHIFY = ["Period", "Geographic Coverage", "Breakdown", "Gas", "Value",]
 for col in ['NC Sector', 'NC Category']:
     try:
         df[col] = df[col].apply(pathify)
     except Exception as err:
         raise Exception('Failed to pathify column "{}".'.format(col)) from err
 
-df = df.drop_duplicates()
+# # +
 
+df = df.replace(
+    {
+        "Breakdown": {
+            "Excluding net emissions/removals from land use, land use change and forestry (LULUCF)": "Excluding net emissions and removals from LULUCF"
+        }
+    }
+)
+
+df["Breakdown"] = df["Breakdown"].str.replace("/", " and ")
+
+df = df.replace({'Gas' : {'Nitrous Oxide N2O' : 'Nitrous oxide (N2O)',
+                          'Methane CH4' : 'Methane (CH4)',
+                          'Carbon Dioxide CO2' :'Carbon dioxide (CO2)'
+                          ''
+                          }})
+
+df['Unit'] = "millions-of-tonnes-of-co2-equivalent"
+df['Measure Type'] = "gas-emissions"
+# -
 df = df[
     [
         "Period",
-        "NC Sector",
-        "NC Category",
+        "Geographic Coverage",
+        "Sector",
         "Gas",
-        "Value",
+        "Breakdown",
+        "Value"
     ]
 ]
 
