cc5a41ef368eca9e10df16dfaf049183007cfdc2
“osamede20” <osamede.asemota@gsscogs.uk>
2023-03-23 13:59:39 +0000

    minor change

--- datasets/DLUHC-Domestic-Energy-Performance-Certificates-for-existing-dwellings-by-energy-efficiency-rating-2022/main.py
+++ datasets/DLUHC-Domestic-Energy-Performance-Certificates-for-existing-dwellings-by-energy-efficiency-rating-2022/main.py
@@ -131,29 +131,29 @@ df['Efficiency Rating'] = df['Efficiency Rating'].apply(pathify)
 df['Measure Type'] = 'energy-performance-certificates'
 df['Unit'] = 'Count'
 
-#Codes for creating local codelist
-g = pd.DataFrame()
+# #Codes for creating local codelist
+# g = pd.DataFrame()
 
-g["Label"] = df["Location Label"].unique()
+# g["Label"] = df["Location Label"].unique()
 
-g["Notation"] = df["Location"].unique()
+# g["Notation"] = df["Location"].unique()
 
-g["Parent Notation"] = None
+# g["Parent Notation"] = None
 
-# g["Local Notation"] = g["Label"].map(lambda x:                            
-#     x if 'E0' in x else 
-#     x if 'W0' in x else 
-#     x if 'E9' in x else 
-#     x if 'W9' in x 
-#     else pathify(x)
-# )
-# g["Local Notation"] = g["Label"]
-g = g.sort_values("Parent Notation", ascending=True)
-g.index += 1
-g["Sort Priority"] = g.index
-g["Description"] = None
+# # g["Local Notation"] = g["Label"].map(lambda x:                            
+# #     x if 'E0' in x else 
+# #     x if 'W0' in x else 
+# #     x if 'E9' in x else 
+# #     x if 'W9' in x 
+# #     else pathify(x)
+# # )
+# # g["Local Notation"] = g["Label"]
+# g = g.sort_values("Parent Notation", ascending=True)
+# g.index += 1
+# g["Sort Priority"] = g.index
+# g["Description"] = None
 
-g.to_csv("./location.csv", index=False)
+# g.to_csv("./location.csv", index=False)
 
 df = df[['Period', 'Location', 'Efficiency Rating', 
          'Measure Type', 'Unit', 'Value']]
