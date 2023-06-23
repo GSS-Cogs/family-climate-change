22179f8c1980d40985caf854cd6118c9822feb85
“osamede20” <osamede.asemota@gsscogs.uk>
2023-06-23 09:10:01 +0000

    major rework

--- datasets/ONS-Energy-use-fossil-fuels-by-fuel-type-and-industry/main.py
+++ datasets/ONS-Energy-use-fossil-fuels-by-fuel-type-and-industry/main.py
@@ -25,126 +25,311 @@ with open("info.json", "r") as read_file:
     data = json.load(read_file)
     title_id = data["id"]
 
+def pathify_section_values(section):
+    if "Total" in section:
+        section = pathify(section)
+        return section
+    if "Travel" in section or "travel" in section:
+        section = pathify(section)
+        return section
+    else:
+        return section
+
 tabs = distribution.as_databaker()
-
-tabs = tabs[2:]
-# for tab in tabs:
-#     print(tab.name, end=", ")
-
-dataframes = []
 for tab in tabs:
-    pivot = tab.excel_ref("A1").shift(0, 4)
-    remove = (
-        tab.filter("Notes").expand(DOWN).expand(RIGHT)
-        | tab.filter("SIC (07) group")
-        | tab.filter("Section")
-    )
-    year = pivot.shift(3, 0).expand(RIGHT).is_not_blank()
-    sicSection = tab.filter("Section").expand(DOWN) - remove | tab.excel_ref(
-        "A5"
-    ).expand(DOWN) - remove.expand(DOWN)
-    sicGroup = tab.filter("SIC (07) group").expand(DOWN) - remove | tab.excel_ref(
-        "B5"
-    ).expand(DOWN) - remove.expand(DOWN)
-    industry = pivot.shift(2, 0).expand(DOWN).is_not_blank() - remove
-
-    if tab.name != "Aviation fuel":
-        observations = industry.fill(RIGHT).is_not_blank()
-
-        if tab.name == "Fuel oil":
-            fueltype = "Fuel oil"
-        elif tab.name == "Gas oil":
-            fueltype = "Gas oil including marine oil excluding DERV"
-        elif tab.name == "Other":
-            fueltype = "Other fuels"
-        else:
-            fueltype = tab.name
-
-        dimensions = [
-            HDim(year, "Year", DIRECTLY, ABOVE),
-            HDim(industry, "Industry", DIRECTLY, LEFT),
-            HDim(sicSection, "SIC Section", DIRECTLY, LEFT),
-            HDim(sicGroup, "SIC Group", DIRECTLY, LEFT),
-            HDimConst("Fuel", fueltype),
-        ]
-        tidy_sheet = ConversionSegment(tab, dimensions, observations)
-        # savepreviewhtml(tidy_sheet,fname=tab.name + "Preview.html")
-        df = tidy_sheet.topandas()
-        df["SIC Group"] = df["SIC Group"].str.rstrip("0")
-        df["SIC Group"] = df["SIC Group"].str.rstrip(".")
-        dataframes.append(df)
-
-    elif tab.name == "Aviation fuel":
-        remove = (
-            tab.filter("Notes").expand(DOWN).expand(RIGHT)
-            | tab.filter("SIC (07) group")
-            | tab.filter("Section")
-        )
-        industry = tab.excel_ref("C6").expand(DOWN) - remove
-        fuel = (
-            tab.excel_ref("D6").expand(DOWN).is_not_blank()
-            - tab.excel_ref("D25").expand(DOWN).is_not_blank()
-        )
-        observations = fuel.fill(RIGHT).is_not_blank() - remove
-
-        dimensions = [
-            HDim(year, "Year", DIRECTLY, ABOVE),
-            HDim(industry, "Industry", DIRECTLY, LEFT),
-            HDim(sicSection, "SIC Section", DIRECTLY, LEFT),
-            HDim(sicGroup, "SIC Group", DIRECTLY, LEFT),
-            HDim(fuel, "Fuel", DIRECTLY, LEFT),
-        ]
-        # savepreviewhtml(tidy_sheet,fname=tab.name + "Preview.html")
-        tidy_sheet = ConversionSegment(tab, dimensions, observations)
-        df = tidy_sheet.topandas()
-        df["SIC Group"] = df["SIC Group"].str.rstrip("0")
-        df["SIC Group"] = df["SIC Group"].str.rstrip(".")
-        # savepreviewhtml(tidy_sheet,fname=tab.name + "Preview.html")
-        dataframes.append(df)
     print(tab.name, end=", ")
 
-df = pd.concat(dataframes)
+tidied_sheets = []
+for tab in tabs:
+    if tab.name in ['Contents', 'Summary ']:
+        continue
+    elif tab.name == 'Carbon based fuels ':
+        #Processing only the top table
+        remove_bottom_section1 = tab.excel_ref("A29").expand(DOWN).expand(RIGHT)
+        year = tab.excel_ref("D5").expand(RIGHT).is_not_blank()
+        section = tab.excel_ref("A6").expand(DOWN) - remove_bottom_section1
+        section_name = tab.excel_ref("C6").expand(DOWN) - remove_bottom_section1
+        observations = section_name.fill(RIGHT).is_not_blank()
+        fuel = "Carbon based fuels"
 
-df.rename(columns={"DATAMARKER": "Marker", "OBS": "Value"}, inplace=True)
+        dimensions = [
+            HDim(section, "Section Notation", DIRECTLY, LEFT),
+            HDim(
+                section_name, "Industry Section Name", DIRECTLY, LEFT
+            ),
+            HDim(year, "Year", DIRECTLY, ABOVE),
+            HDimConst("Fuel", fuel)
+        ]
+        tidy_sheet = ConversionSegment(tab, dimensions, observations)
+        # savepreviewhtml(tidy_sheet, fname = tab.name+ "Preview.html")
+        table = tidy_sheet.topandas()
+        
+        #Creating a unified column for Section Notation and Industry Section Name 
+        table["Section"] = table.apply(
+            lambda x: x["Industry Section Name"]
+            if x["Section Notation"] == "-"
+            else x["Industry Section Name"]
+            if x["Section Notation"] == ""
+            else x["Section Notation"],
+            axis=1,
+        )
+        table["Section"] = table["Section"].apply(pathify_section_values)
+        tidied_sheets.append(table)
+
+    elif tab.name not in ['Aviation fuel', 'Other']:
+        #Processing the top table
+        remove_bottom_section = tab.excel_ref("A29").expand(DOWN).expand(RIGHT)
+        year = tab.excel_ref("D5").expand(RIGHT).is_not_blank()
+        section = tab.excel_ref("A6").expand(DOWN) - remove_bottom_section
+        section_name = tab.excel_ref("C6").expand(DOWN) - remove_bottom_section
+        observations = section_name.fill(RIGHT).is_not_blank()
+        fuel = tab.name
+
+        dimensions = [
+            HDim(section, "Section Notation", DIRECTLY, LEFT), 
+            HDim(
+                section_name, "Industry Section Name", DIRECTLY, LEFT
+            ),  
+            HDim(year, "Year", DIRECTLY, ABOVE),
+            HDimConst("Fuel", fuel)
+        ]
+        tidy_sheet = ConversionSegment(tab, dimensions, observations)
+        # savepreviewhtml(tidy_sheet, fname = tab.name+ "Preview.html")
+        table = tidy_sheet.topandas()
+       
+        #Creating a unified column for Section Notation and Industry Section Name
+        table["Section"] = table.apply(
+            lambda x: x["Industry Section Name"]
+            if x["Section Notation"] == "-"
+            else x["Industry Section Name"]
+            if x["Section Notation"] == ""
+            else x["Section Notation"],
+            axis=1,
+        )
+        table["Section"] = table["Section"].apply(pathify_section_values)
+        tidied_sheets.append(table)
+
+        #Processing the bottom table
+        remove_notes = tab.excel_ref("A162").expand(DOWN).expand(RIGHT)
+
+        sic_group = tab.excel_ref("A31").expand(DOWN) - remove_notes
+        section = tab.excel_ref("B31").expand(DOWN) - remove_notes
+        section_name = tab.excel_ref("C31").expand(DOWN) - remove_notes
+        observations = section_name.fill(RIGHT).is_not_blank()
+        fuel = tab.name
+
+        dimensions = [
+            HDim(sic_group, "SIC(07)Group", DIRECTLY, LEFT),
+            HDim(section, "Section Notation", DIRECTLY, LEFT),  
+            HDim(
+                section_name, "Industry Section Name", DIRECTLY, LEFT
+            ), 
+            HDim(year, "Year", DIRECTLY, ABOVE),
+            HDimConst("Fuel", fuel)
+        ]
+
+        tidy_sheet = ConversionSegment(tab, dimensions, observations)
+        # savepreviewhtml(tidy_sheet, fname = tab.name+ "Preview.html")
+        table = tidy_sheet.topandas()
+
+        #Creating a unified column for SIC(07)Group, Section Notation and Industry Section Name
+        table["Section"] = table.apply(
+            lambda x: x["Industy Section Name"]
+            if x["SIC(07)Group"] == "-"
+            else x["Industry Section Name"]
+            if x["Section Notation"] == ""
+            else x["SIC(07)Group"],
+            axis=1,
+        )
+
+        table["Section"] = table["Section"].str.rstrip("0")
+        table["Section"] = table["Section"].str.rstrip(".")
+        table["Section"] = table["Section"].apply(lambda x: "{0:0>2}".format(x))
+        table["Section"] = table["Section"].apply(pathify_section_values)
+        table["Section"] = table["Section"].apply(pathify)
+        tidied_sheets.append(table)
+
+    elif tab.name == 'Aviation fuel':
+        #Processing the top table
+        remove_bottom_section3 = tab.excel_ref("A14").expand(DOWN).expand(RIGHT)
+        year = tab.excel_ref("E5").expand(RIGHT).is_not_blank()
+        section = tab.excel_ref("A6").expand(DOWN) - remove_bottom_section3
+        section_name = tab.excel_ref("C6").expand(DOWN) - remove_bottom_section3
+        fuel = (
+            tab.excel_ref("D6").expand(DOWN)
+            - remove_bottom_section3 
+        )
+        observations = fuel.fill(RIGHT).is_not_blank()
+        
+        dimensions = [
+            HDim(section, "Section Notation", DIRECTLY, LEFT), 
+            HDim(
+                section_name, "Industry Section Name", DIRECTLY, LEFT
+            ),  
+            HDim(fuel, "Fuel", DIRECTLY, LEFT),
+            HDim(year, "Year", DIRECTLY, ABOVE)   
+        ]
+        tidy_sheet = ConversionSegment(tab, dimensions, observations)
+        # savepreviewhtml(tidy_sheet, fname = tab.name+ "Preview.html")
+        table = tidy_sheet.topandas()
+       
+        #Creating a unified column for Section Notation and Industry Section Name
+        table["Section"] = table.apply(
+            lambda x: x["Industry Section Name"]
+            if x["Section Notation"] == "-"
+            else x["Industry Section Name"]
+            if x["Section Notation"] == ""
+            else x["Section Notation"],
+            axis=1,
+        )
+        table["Section"] = table["Section"].apply(pathify_section_values)
+        tidied_sheets.append(table)
+
+        #Processing the bottom table
+        remove_notes = tab.excel_ref("A25").expand(DOWN).expand(RIGHT)
+
+        sic_group = tab.excel_ref("A18").expand(DOWN) - remove_notes
+        section = tab.excel_ref("B18").expand(DOWN) - remove_notes
+        section_name = tab.excel_ref("C18").expand(DOWN) - remove_notes
+        fuel = (
+            tab.excel_ref("D18").expand(DOWN)
+            - remove_notes
+        )
+        observations = fuel.fill(RIGHT).is_not_blank()
+        
+        dimensions = [
+            HDim(sic_group, "SIC(07)Group", DIRECTLY, LEFT),
+            HDim(section, "Section Notation", DIRECTLY, LEFT),  
+            HDim(
+                section_name, "Industry Section Name", DIRECTLY, LEFT
+            ), 
+            HDim(fuel, "Fuel", DIRECTLY, LEFT),
+            HDim(year, "Year", DIRECTLY, ABOVE) 
+        ]
+
+        tidy_sheet = ConversionSegment(tab, dimensions, observations)
+        # savepreviewhtml(tidy_sheet, fname = tab.name+ "Preview.html")
+        table = tidy_sheet.topandas()
+
+        #Creating a unified column for SIC(07)Group, Section Notation and Industry Section Name
+        table["Section"] = table.apply(
+            lambda x: x["Industy Section Name"]
+            if x["SIC(07)Group"] == "-"
+            else x["Industry Section Name"]
+            if x["Section Notation"] == ""
+            else x["SIC(07)Group"],
+            axis=1,
+        )
+
+        table["Section"] = table["Section"].str.rstrip("0")
+        table["Section"] = table["Section"].str.rstrip(".")
+        table["Section"] = table["Section"].apply(lambda x: "{0:0>2}".format(x))
+        table["Section"] = table["Section"].apply(pathify_section_values)
+        table["Section"] = table["Section"].apply(pathify)
+        tidied_sheets.append(table)
+    
+    elif tab.name == 'Other':
+        #Processing the middle table
+     
+        remove_bottom_section = tab.excel_ref("A51").expand(DOWN).expand(RIGHT)
+        year = tab.excel_ref("D5").expand(RIGHT).is_not_blank()
+        section = tab.excel_ref("A28").expand(DOWN) - remove_bottom_section
+        section_name = tab.excel_ref("C28").expand(DOWN) - remove_bottom_section
+        fuel = 'Other fuels'
+        observations = section_name.fill(RIGHT).is_not_blank()
+        
+        dimensions = [
+            HDim(section, "Section Notation", DIRECTLY, LEFT), 
+            HDim(
+                section_name, "Industry Section Name", DIRECTLY, LEFT
+            ),  
+            HDim(year, "Year", DIRECTLY, ABOVE),
+            HDimConst("Fuel", fuel)   
+        ]
+        tidy_sheet = ConversionSegment(tab, dimensions, observations)
+        # savepreviewhtml(tidy_sheet, fname = tab.name+ "Preview.html")
+        table = tidy_sheet.topandas()
+       
+        #Creating a unified column for Section Notation and Industry Section Name
+        table["Section"] = table.apply(
+            lambda x: x["Industry Section Name"]
+            if x["Section Notation"] == "-"
+            else x["Industry Section Name"]
+            if x["Section Notation"] == ""
+            else x["Section Notation"],
+            axis=1,
+        )
+        table["Section"] = table["Section"].apply(pathify_section_values)
+        tidied_sheets.append(table)
+
+        #Processing the bottom table
+        remove_notes = tab.excel_ref("A184").expand(DOWN).expand(RIGHT)
+
+        sic_group = tab.excel_ref("A53").expand(DOWN) - remove_notes
+        section = tab.excel_ref("B53").expand(DOWN) - remove_notes
+        section_name = tab.excel_ref("C53").expand(DOWN) - remove_notes
+        fuel = 'Other fuels'
+        observations = section_name.fill(RIGHT).is_not_blank()
+        
+        dimensions = [
+            HDim(sic_group, "SIC(07)Group", DIRECTLY, LEFT),
+            HDim(section, "Section Notation", DIRECTLY, LEFT),  
+            HDim(
+                section_name, "Industry Section Name", DIRECTLY, LEFT
+            ), 
+            HDim(year, "Year", DIRECTLY, ABOVE),
+            HDimConst("Fuel", fuel), 
+        ]
+
+        tidy_sheet = ConversionSegment(tab, dimensions, observations)
+        # savepreviewhtml(tidy_sheet, fname = tab.name+ "Preview.html")
+        table = tidy_sheet.topandas()
+
+        #Creating a unified column for SIC(07)Group, Section Notation and Industry Section Name
+        table["Section"] = table.apply(
+            lambda x: x["Industy Section Name"]
+            if x["SIC(07)Group"] == "-"
+            else x["Industry Section Name"]
+            if x["Section Notation"] == ""
+            else x["SIC(07)Group"],
+            axis=1,
+        )
+
+        table["Section"] = table["Section"].str.rstrip("0")
+        table["Section"] = table["Section"].str.rstrip(".")
+        table["Section"] = table["Section"].apply(lambda x: "{0:0>2}".format(x))
+        table["Section"] = table["Section"].apply(pathify_section_values)
+        table["Section"] = table["Section"].apply(pathify)
+        tidied_sheets.append(table)
+    
+    print(tab.name)
+
+df = pd.concat(tidied_sheets, sort=True)
+
+df.rename(columns={"OBS": "Value", "DATAMARKER": "Marker"}, inplace=True)
+df["Year"] = df["Year"].astype(str).replace("\.0", "", regex=True)
+
 df.replace(
     {
         "Marker": {"c": "confidential"},
-        "Fuel": {"('DERV',)": "DERV", "Carbon based fuels ": "Carbon based fuels"},
+        "Fuel": {"Carbon based fuels ": "Carbon based fuels",
+                "('DERV',)": "Derv", 
+                "Derv": "Diesel oil for road vehicles (DERV)",
+                "Gas oil": "Gas oil including marine oil excluding DERV"},
+        'Section': {'total': 'grand-total',
+                   "Consumer expenditure": "consumer-expenditure", }
     },
     inplace=True,
 )
 
-# df["Fuel"] = df["Fuel"].str.rstrip(" ")
-
-df.replace(
-    {
-        "Fuel": {"DERV": "Diesel oil for road vehicles (DERV)"},
-    },
-    inplace=True,
-)
-
+df["Fuel"] = df["Fuel"].str.rstrip(" ")
 df["Year"] = df["Year"].astype(float).astype(int)
-df["Industry"] = df["Industry"].apply(pathify)
-df["SIC Group"] = df["SIC Group"].apply(pathify)
 
-df["SIC Group"] = df.apply(
-    lambda x: x["SIC Section"]
-    if x["SIC Group"] == "" or x["SIC Group"] == "-"
-    else x["SIC Group"],
-    axis=1,
-)
-df["SIC Group"] = df.apply(
-    lambda x: x["Industry"]
-    if x["SIC Group"] == "-" and x["SIC Section"] == "-"
-    else x["SIC Group"],
-    axis=1,
-)
-df["SIC Group"] = df.apply(
-    lambda x: x["Industry"]
-    if x["SIC Group"] == "" and x["SIC Section"] == ""
-    else x["SIC Group"],
-    axis=1,
-)
+indexNames = df[df['Section'] == ''].index
+df.drop(indexNames, inplace=True)
+
+indexNames = df[df['Fuel'] == ''].index
+df.drop(indexNames, inplace=True)
 
 # info needed to create URI's for section
 unique = (
@@ -153,26 +338,18 @@ unique = (
     + "-concept/sic-2007/"
 )
 sic = "http://business.data.gov.uk/companies/def/sic-2007/"
-
 # create the URI's from the section column
-df["SIC Group"] = df["SIC Group"].map(
-    lambda x: unique + x if "-" in x else (unique + x if "total" in x else sic + x)
+
+df["Section"] = df["Section"].map(
+    lambda x: unique + x if "-" in x else (unique + x if "grand-total" in x else sic + x)
 )
-df.drop(["SIC Section", "Industry"], axis=1, inplace=True)
 
-df.rename(columns={"SIC Group": "Section", "Period": "Year"}, inplace=True)
+try:
+    df['Fuel'] = df['Fuel'].apply(pathify)
+except Exception as err:
+    raise Exception('Failed to pathify column "{}".'.format(df['Fuel'])) from err
+
 df = df[["Year", "Section", "Fuel", "Value", "Marker"]]
-
-COLUMNS_TO_NOT_PATHIFY = ["Year", "Section", "Value", "Marker"]
-for col in df.columns.values.tolist():
-    if col in COLUMNS_TO_NOT_PATHIFY:
-        continue
-    try:
-        df[col] = df[col].apply(pathify)
-    except Exception as err:
-        raise Exception('Failed to pathify column "{}".'.format(col)) from err
-
-df = df.drop_duplicates()
 
 df.to_csv("observations.csv", index=False)
 catalog_metadata = metadata.as_csvqb_catalog_metadata()
