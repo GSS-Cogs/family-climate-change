f35c24c57d09369d57da328ff4aaf6a5bd025be1
“osamede20” <osamede.asemota@gsscogs.uk>
2023-07-20 09:40:57 +0000

    changes to metadata

--- datasets/Met-Office-Annual-growing-season-length-with-trends-actual/main.py
+++ datasets/Met-Office-Annual-growing-season-length-with-trends-actual/main.py
@@ -13,8 +13,9 @@ df.to_csv('observations.csv', index=False)
 catalog_metadata = CatalogMetadata(
     title="Annual growing season length with trends actual",
     description="Priority dataset for Climate Change Platform project.",
-    license_uri=["https://www.nationalarchives.gov.uk/doc/open-government-licence/version/3/"],
-    creator_uri=["https://www.gov.uk/government/organisations/the-meteorological-office"]
+    license_uri="https://www.nationalarchives.gov.uk/doc/open-government-licence/version/3/",
+    creator_uri="https://www.gov.uk/government/organisations/the-meteorological-office",
+    theme_uris=["http://gss-data.org.uk/def/gdp#climate-change"]
 )
 catalog_metadata.to_json_file('catalog-metadata.json')
 
